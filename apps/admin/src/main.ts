c7a3905e9f1cbf8aa91e84a321d66ba530d6da00
jongwon-choi <jongwon.choi@balaan.co.kr>
2024-09-06 12:01:57 +0900

    chore: admin main versioning 설정

--- apps/admin/src/main.ts
+++ apps/admin/src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AdminModule } from './admin.module';
+import { VersioningType } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AdminModule);
+
+  app.enableVersioning({
+    type: VersioningType.URI,
+  });
+
   await app.listen(3000);
 }
 bootstrap();
