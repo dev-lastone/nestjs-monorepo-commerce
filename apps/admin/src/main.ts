435e376653431ae1e2ffd5903ee636c8a1b4ff52
jongwon-choi <jongwon.choi@balaan.co.kr>
2024-09-06 12:02:10 +0900

    chore: admin main swagger 설정

--- apps/admin/src/main.ts
+++ apps/admin/src/main.ts
@@ -1,12 +1,28 @@
 import { NestFactory } from '@nestjs/core';
 import { AdminModule } from './admin.module';
 import { VersioningType } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AdminModule);
 
   app.enableVersioning({
     type: VersioningType.URI,
+  });
+
+  const options = new DocumentBuilder()
+    .setTitle('ADMIN API')
+    .setDescription('ADMIN API 문서입니다.')
+    .setVersion('1.0.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('docs', app, document, {
+    swaggerOptions: {
+      persistAuthorization: true,
+      defaultModelsExpandDepth: -1,
+      defaultModelExpandDepth: 10,
+    },
   });
 
   await app.listen(3000);
