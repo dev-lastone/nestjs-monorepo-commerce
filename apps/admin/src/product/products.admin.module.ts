d00e64785261864cea33a5e2e157adaba33d7c50
jongwon-choi <jongwon.choi@balaan.co.kr>
2024-09-06 12:04:26 +0900

    refactor: ProductsAdminModule 미사용 코드 제거

--- apps/admin/src/product/products.admin.module.ts
+++ apps/admin/src/product/products.admin.module.ts
@@ -3,7 +3,6 @@ import { ProductsAdminController } from './products.admin.controller';
 import { ProductsAdminService } from './products.admin.service';
 
 @Module({
-  imports: [],
   controllers: [ProductsAdminController],
   providers: [ProductsAdminService],
 })
