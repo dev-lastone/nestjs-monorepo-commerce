6570531bfdd72fcb9ee741baa2ca94e0aa1dd628
jongwon-choi <jongwon.choi@balaan.co.kr>
2024-09-13 16:26:56 +0900

    feat: admin auth signIn jwt token 반환 로직 추가

--- apps/admin/src/auth/auth.admin.module.ts
+++ apps/admin/src/auth/auth.admin.module.ts
@@ -1,8 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AuthAdminController } from './auth.admin.controller';
 import { AuthAdminService } from './auth.admin.service';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
+  imports: [
+    JwtModule.register({
+      secret: 'test',
+      signOptions: { expiresIn: '60s' },
+    }),
+  ],
   controllers: [AuthAdminController],
   providers: [AuthAdminService],
 })
