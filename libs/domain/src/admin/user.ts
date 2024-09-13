38b84c715367fcff5138248d62d4cb5a46ac9d2e
jjong <90lastone@gmail.com>
2024-09-13 12:51:00 +0900

    feat: user email, password 필수 형식 검증 데코레이터 추가

--- libs/domain/src/admin/user.ts
+++ libs/domain/src/admin/user.ts
@@ -1,10 +1,14 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { IsEmail, IsNotEmpty } from 'class-validator';
 
 export class User {
   id: number;
   name: string;
   @ApiProperty({ default: 'test@test.com' })
+  @IsEmail()
+  @IsNotEmpty()
   email: string;
   @ApiProperty({ default: '1234' })
+  @IsNotEmpty()
   password: string;
 }
