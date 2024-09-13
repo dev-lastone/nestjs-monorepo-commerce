73a82786a3db849053f5edf0cc62731bc60ce5cb
jongwon-choi <jongwon.choi@balaan.co.kr>
2024-09-13 12:51:24 +0900

    test: AuthAdminController signIn 값 체크

--- apps/admin/src/auth/auth.admin.controller.spec.ts
+++ apps/admin/src/auth/auth.admin.controller.spec.ts
@@ -2,10 +2,15 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { AuthAdminController } from './auth.admin.controller';
 import { AuthAdminService } from './auth.admin.service';
 import { PostAuthAdminRequestDto } from './auth.admin.dto';
-import { UnauthorizedException } from '@nestjs/common';
+import {
+  BadRequestException,
+  UnauthorizedException,
+  ValidationPipe,
+} from '@nestjs/common';
 
 describe('AuthAdminController', () => {
   let authAdminController: AuthAdminController;
+  let validationPipe: ValidationPipe;
 
   beforeEach(async () => {
     const app: TestingModule = await Test.createTestingModule({
@@ -14,9 +19,52 @@ describe('AuthAdminController', () => {
     }).compile();
 
     authAdminController = app.get<AuthAdminController>(AuthAdminController);
+
+    validationPipe = new ValidationPipe();
   });
 
   describe('signIn', () => {
+    describe('값 체크', () => {
+      it('email 필수', async () => {
+        const postAuthAdminRequestDto = new PostAuthAdminRequestDto();
+        postAuthAdminRequestDto.email = '';
+        postAuthAdminRequestDto.password = '1234';
+
+        await expect(
+          validationPipe.transform(postAuthAdminRequestDto, {
+            type: 'body',
+            metatype: PostAuthAdminRequestDto,
+          }),
+        ).rejects.toThrow(BadRequestException);
+      });
+
+      it('password 필수', async () => {
+        const postAuthAdminRequestDto = new PostAuthAdminRequestDto();
+        postAuthAdminRequestDto.email = 'test@test.com';
+        postAuthAdminRequestDto.password = '';
+
+        await expect(
+          validationPipe.transform(postAuthAdminRequestDto, {
+            type: 'body',
+            metatype: PostAuthAdminRequestDto,
+          }),
+        ).rejects.toThrow(BadRequestException);
+      });
+
+      it('이메일 형식', async () => {
+        const postAuthAdminRequestDto = new PostAuthAdminRequestDto();
+        postAuthAdminRequestDto.email = 'test.com';
+        postAuthAdminRequestDto.password = '1234';
+
+        await expect(
+          validationPipe.transform(postAuthAdminRequestDto, {
+            type: 'body',
+            metatype: PostAuthAdminRequestDto,
+          }),
+        ).rejects.toThrow(BadRequestException);
+      });
+    });
+
     it('잘못된 이메일', () => {
       const postAuthAdminRequestDto = new PostAuthAdminRequestDto();
       postAuthAdminRequestDto.email = 'invalid@test.com';
