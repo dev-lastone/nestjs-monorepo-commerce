38415f7861aeccfff0299fe573420dd7d379076e
jjong <akrTmwk@gmail.com>
2024-09-18 16:39:25 +0900

    test: validateEnv JWT_EXPIRES_IN 추가

--- apps/admin/src/config/_test/env.validation.spec.ts
+++ apps/admin/src/config/_test/env.validation.spec.ts
@@ -4,13 +4,16 @@ import { EnvVariables, validateEnv } from '../env.validation';
 describe('env validation', () => {
   it('성공', () => {
     const JWT_SECRET = 'test';
+    const JWT_EXPIRES_IN = '60s';
 
     const config = {
       JWT_SECRET,
+      JWT_EXPIRES_IN,
     };
 
     const envVariables = new EnvVariables();
     envVariables.JWT_SECRET = JWT_SECRET;
+    envVariables.JWT_EXPIRES_IN = JWT_EXPIRES_IN;
 
     expect(validateEnv(config)).toEqual(envVariables);
   });
@@ -21,6 +24,10 @@ describe('env validation', () => {
       {
         isNotEmpty: 'JWT_SECRET should not be empty',
         isString: 'JWT_SECRET must be a string',
+      },
+      {
+        isNotEmpty: 'JWT_EXPIRES_IN should not be empty',
+        isString: 'JWT_EXPIRES_IN must be a string',
       },
     ];
 
