0026f6a90de09cf9fe0660062b3363d90814cec2
jjong <akrTmwk@gmail.com>
2024-09-18 16:42:33 +0900

    feat: admin Public decorator 적용

--- apps/admin/src/auth/auth.admin.controller.ts
+++ apps/admin/src/auth/auth.admin.controller.ts
@@ -2,12 +2,14 @@ import { Body, Controller, Post, Version } from '@nestjs/common';
 import { AuthAdminService } from './auth.admin.service';
 import { PostAuthAdminRequestDto } from './auth.admin.dto';
 import { ApiTags } from '@nestjs/swagger';
+import { Public } from '../decorator/public.decorator';
 
 @ApiTags('auth')
 @Controller('auth')
 export class AuthAdminController {
   constructor(private readonly authAdminService: AuthAdminService) {}
 
+  @Public()
   @Version('1')
   @Post()
   async signIn(@Body() dto: PostAuthAdminRequestDto) {
