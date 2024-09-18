7767ebcf560fc614959b39b67446cdef31b2ac28
jjong <akrTmwk@gmail.com>
2024-09-18 16:03:52 +0900

    test: JwtStrategy configService 의존성 해결

--- apps/admin/src/auth/_test/jwt.strategy.spec.ts
+++ apps/admin/src/auth/_test/jwt.strategy.spec.ts
@@ -1,7 +1,30 @@
 import { JwtStrategy } from '../jwt.strategy';
+import { ConfigService } from '@nestjs/config';
+import { Test, TestingModule } from '@nestjs/testing';
 
 describe('JwtAuthGuard', () => {
-  const jwtStrategy = new JwtStrategy();
+  let jwtStrategy: JwtStrategy;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        JwtStrategy,
+        {
+          provide: ConfigService,
+          useValue: {
+            get: jest.fn().mockImplementation((key: string) => {
+              switch (key) {
+                case 'JWT_SECRET':
+                  return 'secret'; // 테스트 환경에서 사용할 값
+              }
+            }),
+          },
+        },
+      ],
+    }).compile();
+
+    jwtStrategy = module.get<JwtStrategy>(JwtStrategy);
+  });
 
   it('validate', () => {
     const jwtPayload = {
