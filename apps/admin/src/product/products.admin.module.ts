e63a92672df4caf6f52b1b4d4617a056455194a8
jjong <akrTmwk@gmail.com>
2024-09-18 15:33:35 +0900

    refactor: AuthAdminGuard -> JwtAuthGuard 로 변경

--- apps/admin/src/product/products.admin.module.ts
+++ apps/admin/src/product/products.admin.module.ts
@@ -1,15 +1,8 @@
 import { Module } from '@nestjs/common';
 import { ProductsAdminController } from './products.admin.controller';
 import { ProductsAdminService } from './products.admin.service';
-import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  imports: [
-    JwtModule.register({
-      secret: 'test',
-      signOptions: { expiresIn: '60s' },
-    }),
-  ],
   controllers: [ProductsAdminController],
   providers: [ProductsAdminService],
 })
