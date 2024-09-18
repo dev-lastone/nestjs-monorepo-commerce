e89395d49bcdb298e8cf578aacc9043274e897de
jjong <akrTmwk@gmail.com>
2024-09-18 16:40:47 +0900

    feat: JwtAuthGuard admin 글로벌 적용

--- apps/admin/src/admin.module.ts
+++ apps/admin/src/admin.module.ts
@@ -5,6 +5,8 @@ import { ProductsAdminModule } from './product/products.admin.module';
 import { AuthAdminModule } from './auth/auth.admin.module';
 import { ConfigModule } from '@nestjs/config';
 import { validateEnv } from './config/env.validation';
+import { APP_GUARD } from '@nestjs/core';
+import { JwtAuthGuard } from './auth/jwt.auth.guard';
 
 @Module({
   imports: [
@@ -16,6 +18,12 @@ import { validateEnv } from './config/env.validation';
     ProductsAdminModule,
   ],
   controllers: [AdminController],
-  providers: [AdminService],
+  providers: [
+    AdminService,
+    {
+      provide: APP_GUARD,
+      useClass: JwtAuthGuard,
+    },
+  ],
 })
 export class AdminModule {}
