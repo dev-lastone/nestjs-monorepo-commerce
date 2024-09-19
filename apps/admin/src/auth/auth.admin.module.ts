7a30c39826ba744679942abc89c39b9f7c895fef
jongwon-choi <jongwon.choi@balaan.co.kr>
2024-09-19 17:35:07 +0900

    refactor: AuthAdminModule PassportModule, JwtModule 미사용 제거

--- apps/admin/src/auth/auth.admin.module.ts
+++ apps/admin/src/auth/auth.admin.module.ts
@@ -1,27 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AuthAdminService } from './auth.admin.service';
-import { JwtModule } from '@nestjs/jwt';
-import { PassportModule } from '@nestjs/passport';
-import { JwtStrategy } from './jwt.strategy';
 import { AuthAdminController } from './auth.admin.controller';
-import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [
-    PassportModule,
-    JwtModule.registerAsync({
-      imports: [ConfigModule],
-      inject: [ConfigService],
-      useFactory: async (configService: ConfigService) => ({
-        secret: configService.get<string>('JWT_SECRET'),
-        signOptions: {
-          expiresIn: configService.get<string | number>('JWT_EXPIRES_IN'),
-        },
-      }),
-    }),
-  ],
   controllers: [AuthAdminController],
-  providers: [AuthAdminService, JwtStrategy],
-  exports: [AuthAdminService],
+  providers: [AuthAdminService],
 })
 export class AuthAdminModule {}
