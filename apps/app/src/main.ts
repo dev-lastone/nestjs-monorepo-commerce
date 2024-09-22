08cb6056be0ee7568b332303a1acf0d9b5542837
jjong <akrTmwk@gmail.com>
2024-09-22 21:49:27 +0900

    chore: app main swagger, validationPipe, versioning 설정

--- apps/app/src/main.ts
+++ apps/app/src/main.ts
@@ -1,8 +1,41 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe, VersioningType } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableVersioning({
+    type: VersioningType.URI,
+  });
+
+  app.useGlobalPipes(new ValidationPipe());
+
+  const options = new DocumentBuilder()
+    .setTitle('APP API')
+    .setDescription('APP API 문서입니다.')
+    .setVersion('1.0.0')
+    .addBearerAuth(
+      {
+        type: 'http',
+        scheme: 'bearer',
+        name: 'Authorization',
+        in: 'header',
+      },
+      'jwt',
+    )
+    .build();
+
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('docs', app, document, {
+    swaggerOptions: {
+      persistAuthorization: true,
+      defaultModelsExpandDepth: -1,
+      defaultModelExpandDepth: 10,
+    },
+  });
+
   await app.listen(3000);
 }
 bootstrap();
