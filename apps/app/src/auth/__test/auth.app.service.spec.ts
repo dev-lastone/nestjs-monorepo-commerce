982b36095105adc838a1aa0566c1a9c4f707f66b
jongwon-choi <jongwon.choi@balaan.co.kr>
2024-09-23 18:55:22 +0900

    test: AuthAppService

---
+++ apps/app/src/auth/__test/auth.app.service.spec.ts
@@ -0,0 +1,63 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { UnauthorizedException } from '@nestjs/common';
+import { AuthService } from '@domain/domain/auth/auth.service';
+import { AuthAppService } from '../auth.app.service';
+import { PostAuthAppRequestDto } from '../auth.app.dto';
+
+describe('AuthAppService', () => {
+  let authAppService: AuthAppService;
+  let authService: AuthService;
+
+  beforeEach(async () => {
+    const app: TestingModule = await Test.createTestingModule({
+      providers: [
+        AuthAppService,
+        {
+          provide: AuthService,
+          useValue: {
+            createToken: jest.fn(),
+          },
+        },
+      ],
+    }).compile();
+
+    authAppService = app.get<AuthAppService>(AuthAppService);
+    authService = app.get<AuthService>(AuthService);
+  });
+
+  describe('signIn', () => {
+    it('잘못된 이메일', async () => {
+      const postAuthAppRequestDto = new PostAuthAppRequestDto();
+      postAuthAppRequestDto.email = 'invalid@test.com';
+      postAuthAppRequestDto.password = '1234';
+
+      expect(() => authAppService.signIn(postAuthAppRequestDto)).toThrow(
+        new UnauthorizedException(),
+      );
+    });
+
+    it('잘못된 패스워드', async () => {
+      const postAuthAdminRequestDto = new PostAuthAppRequestDto();
+      postAuthAdminRequestDto.email = 'test@test.com';
+      postAuthAdminRequestDto.password = 'invalid';
+
+      expect(() => authAppService.signIn(postAuthAdminRequestDto)).toThrow(
+        new UnauthorizedException(),
+      );
+    });
+
+    it('성공', async () => {
+      const postAuthAdminRequestDto = new PostAuthAppRequestDto();
+      postAuthAdminRequestDto.email = 'test@test.com';
+      postAuthAdminRequestDto.password = '1234';
+
+      jest.spyOn(authService, 'createToken').mockReturnValue('mockToken');
+
+      const result = authAppService.signIn(postAuthAdminRequestDto);
+
+      expect(result).toEqual({
+        token: 'mockToken',
+      });
+    });
+  });
+});
