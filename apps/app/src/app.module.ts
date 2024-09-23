b2e2ab2e405c24a6ea2ac9f125c6aa5a053a56db
jongwon-choi <jongwon.choi@balaan.co.kr>
2024-09-23 15:07:10 +0900

    refactor: ConfigModule 함수 추출

--- apps/app/src/app.module.ts
+++ apps/app/src/app.module.ts
@@ -2,19 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthAppModule } from './auth/auth.app.module';
-import { ConfigModule } from '@nestjs/config';
-import { validateEnv } from '../../admin/src/config/env.validation';
-import { join } from 'path';
+import { configModule } from '@common/common/setting/config';
 
 @Module({
-  imports: [
-    ConfigModule.forRoot({
-      isGlobal: true,
-      validate: validateEnv,
-      envFilePath: join(__dirname, '../../../.env'),
-    }),
-    AuthAppModule,
-  ],
+  imports: [configModule(), AuthAppModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ libs/common/src/setting/config.ts
@@ -0,0 +1,11 @@
+import { ConfigModule } from '@nestjs/config';
+import { validateEnv } from '../../../../apps/admin/src/config/env.validation';
+import { join } from 'path';
+
+export function configModule() {
+  return ConfigModule.forRoot({
+    isGlobal: true,
+    validate: validateEnv,
+    envFilePath: join(__dirname, '../../../.env'),
+  });
+}
