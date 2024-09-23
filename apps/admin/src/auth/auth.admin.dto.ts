19f61de2d4b982cb1b9c72098ae965ce513cbbfa
jjong <90lastone@gmail.com>
2024-09-23 18:41:44 +0900

    refactor: User -> AdminUser

--- apps/admin/src/auth/auth.admin.dto.ts
+++ apps/admin/src/auth/auth.admin.dto.ts
@@ -1,7 +1,7 @@
 import { PickType } from '@nestjs/swagger';
-import { User } from '@domain/domain/admin/user';
+import { AdminUser } from '@domain/domain/admin/user';
 
-export class PostAuthAdminRequestDto extends PickType(User, [
+export class PostAuthAdminRequestDto extends PickType(AdminUser, [
   'email',
   'password',
 ]) {}
