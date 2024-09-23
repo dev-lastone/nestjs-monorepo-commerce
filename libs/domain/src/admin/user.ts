d9cca0433ae8c62532e9257fad8ffac8d07d3bc2
jongwon-choi <jongwon.choi@balaan.co.kr>
2024-09-23 18:41:44 +0900

    refactor: User -> AdminUser

--- libs/domain/src/admin/user.ts
+++ libs/domain/src/admin/user.ts
@@ -1,7 +1,7 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { IsEmail, IsNotEmpty } from 'class-validator';
 
-export class User {
+export class AdminUser {
   id: number;
   name: string;
   @ApiProperty({ default: 'test@test.com' })
