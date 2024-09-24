dd2e9a50c25f3368fef67dd1cab79ac76490c845
jongwon-choi <90lastone@gmail.com>
2024-09-24 18:34:39 +0900

    test: ProductService checkExistentProduct

---
+++ libs/domain/src/product/__test/product.service.spec.ts
@@ -0,0 +1,27 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { ProductService } from '@domain/domain/product/product.service';
+import { NotFoundException } from '@nestjs/common';
+
+describe('ProductService', () => {
+  let productService: ProductService;
+
+  beforeEach(async () => {
+    const app: TestingModule = await Test.createTestingModule({
+      providers: [ProductService],
+    }).compile();
+
+    productService = app.get<ProductService>(ProductService);
+  });
+
+  describe('checkExistentProduct', () => {
+    it('실패', () => {
+      expect(() => productService.checkExistentProduct(0)).toThrow(
+        new NotFoundException(),
+      );
+    });
+
+    it('성공', () => {
+      expect(productService.checkExistentProduct(1)).toBe(0);
+    });
+  });
+});
