2a91cd56db8be3f9bd8f869b4439008f00b99484
jongwon-choi <90lastone@gmail.com>
2024-09-24 15:42:04 +0900

    feat: ProductsAppController getProducts @Public 적용

--- apps/app/src/product/products.app.controller.ts
+++ apps/app/src/product/products.app.controller.ts
@@ -2,12 +2,14 @@ import { Controller, Get, Version } from '@nestjs/common';
 import { Product } from '@domain/domain/product/product';
 import { ApiTags } from '@nestjs/swagger';
 import { ProductService } from '@domain/domain/product/product.service';
+import { Public } from '@common/common/decorator/public.decorator';
 
 @ApiTags('products')
 @Controller('products')
 export class ProductsAppController {
   constructor(private readonly productService: ProductService) {}
 
+  @Public()
   @Version('1')
   @Get()
   getProducts(): Product[] {
