1fde2ace117b4184df67061dc9dad43da8bb11ca
jjong <90lastone@gmail.com>
2024-09-24 11:26:54 +0900

    refactor: AdminProductsService domain ProductService 로 변경

--- apps/admin/src/product/products.admin.controller.ts
+++ apps/admin/src/product/products.admin.controller.ts
@@ -9,44 +9,48 @@ import {
   Put,
   Version,
 } from '@nestjs/common';
-import { ProductsAdminService } from './products.admin.service';
-import {
-  PostProductAdminRequestDto,
-  PutProductAdminRequestDto,
-} from './products.admin.dto';
 import { Product } from '@domain/domain/product/product';
 import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
+import { ProductService } from '@domain/domain/product/product.service';
+import {
+  CreateProductDto,
+  UpdateProductDto,
+} from '@domain/domain/product/product.dto';
 
 @ApiBearerAuth('jwt')
 @ApiTags('products')
 @Controller('products')
 export class ProductsAdminController {
-  constructor(private readonly adminService: ProductsAdminService) {}
+  constructor(private readonly productService: ProductService) {}
 
   @Version('1')
   @Post()
-  postProduct(@Body() dto: PostProductAdminRequestDto): Product {
-    return this.adminService.postProduct(dto);
+  postProduct(@Body() dto: CreateProductDto): Product {
+    return this.productService.createProduct(dto);
   }
 
   @Version('1')
   @Get()
   getProducts(): Product[] {
-    return this.adminService.getProducts();
+    return this.productService.findProducts();
   }
 
   @Version('1')
   @Put(':id')
   putProduct(
     @Param('id', new ParseIntPipe()) id: number,
-    @Body() dto: PutProductAdminRequestDto,
+    @Body() dto: UpdateProductDto,
   ): Product {
-    return this.adminService.putProduct(id, dto);
+    return this.productService.updateProduct({
+      id,
+      name: dto.name,
+      price: dto.price,
+    });
   }
 
   @Version('1')
   @Delete(':id')
   deleteProduct(@Param('id', new ParseIntPipe()) id: number) {
-    this.adminService.deleteProduct(id);
+    this.productService.deleteProduct(id);
   }
 }
