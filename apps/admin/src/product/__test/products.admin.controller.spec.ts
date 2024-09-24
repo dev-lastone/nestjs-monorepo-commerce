0402ae58c3b525676c0ff6025cd95a0e934dcc2b
jjong <90lastone@gmail.com>
2024-09-24 11:11:13 +0900

    refactor: products.admin.controller.spec.ts 의미없는 describe 제거

--- apps/admin/src/product/__test/products.admin.controller.spec.ts
+++ apps/admin/src/product/__test/products.admin.controller.spec.ts
@@ -7,7 +7,7 @@ import {
   PutProductAdminRequestDto,
 } from '../products.admin.dto';
 
-describe('AdminController', () => {
+describe('ProductsAdminController', () => {
   const NON_EXISTENT_ID = Number.MAX_SAFE_INTEGER;
 
   let productsAdminController: ProductsAdminController;
@@ -23,70 +23,68 @@ describe('AdminController', () => {
     );
   });
 
-  describe('products', () => {
-    it('post', () => {
-      const postProductAdminRequestDto = new PostProductAdminRequestDto();
-      postProductAdminRequestDto.name = 'test2';
-      postProductAdminRequestDto.price = 20000;
+  it('post', () => {
+    const postProductAdminRequestDto = new PostProductAdminRequestDto();
+    postProductAdminRequestDto.name = 'test2';
+    postProductAdminRequestDto.price = 20000;
+
+    expect(
+      productsAdminController.postProduct(postProductAdminRequestDto),
+    ).toEqual({
+      id: 2,
+      ...postProductAdminRequestDto,
+    });
+  });
+
+  it('get', () => {
+    expect(productsAdminController.getProducts()).toEqual([
+      {
+        id: 1,
+        name: 'test1',
+        price: 10000,
+      },
+    ]);
+  });
+
+  describe('put', () => {
+    const putProductAdminRequestDto = new PutProductAdminRequestDto();
+    putProductAdminRequestDto.name = '상품2';
+    putProductAdminRequestDto.price = 15000;
+
+    it('404', () => {
+      expect(() =>
+        productsAdminController.putProduct(
+          NON_EXISTENT_ID,
+          putProductAdminRequestDto,
+        ),
+      ).toThrow(new NotFoundException());
+    });
+
+    it('200', () => {
+      const id = 1;
 
       expect(
-        productsAdminController.postProduct(postProductAdminRequestDto),
+        productsAdminController.putProduct(id, putProductAdminRequestDto),
       ).toEqual({
-        id: 2,
-        ...postProductAdminRequestDto,
+        id,
+        ...putProductAdminRequestDto,
       });
     });
+  });
 
-    it('get', () => {
-      expect(productsAdminController.getProducts()).toEqual([
-        {
-          id: 1,
-          name: 'test1',
-          price: 10000,
-        },
-      ]);
+  describe('delete', () => {
+    it('404', () => {
+      expect(() =>
+        productsAdminController.deleteProduct(NON_EXISTENT_ID),
+      ).toThrow(new NotFoundException());
     });
 
-    describe('put', () => {
-      const putProductAdminRequestDto = new PutProductAdminRequestDto();
-      putProductAdminRequestDto.name = '상품2';
-      putProductAdminRequestDto.price = 15000;
+    it('200', () => {
+      const id = 1;
 
-      it('404', () => {
-        expect(() =>
-          productsAdminController.putProduct(
-            NON_EXISTENT_ID,
-            putProductAdminRequestDto,
-          ),
-        ).toThrow(new NotFoundException());
-      });
+      productsAdminController.deleteProduct(id);
 
-      it('200', () => {
-        const id = 1;
-
-        expect(
-          productsAdminController.putProduct(id, putProductAdminRequestDto),
-        ).toEqual({
-          id,
-          ...putProductAdminRequestDto,
-        });
-      });
-    });
-
-    describe('delete', () => {
-      it('404', () => {
-        expect(() =>
-          productsAdminController.deleteProduct(NON_EXISTENT_ID),
-        ).toThrow(new NotFoundException());
-      });
-
-      it('200', () => {
-        const id = 1;
-
-        productsAdminController.deleteProduct(id);
-
-        expect(productsAdminController.getProducts()).toEqual([]);
-      });
+      expect(productsAdminController.getProducts()).toEqual([]);
     });
   });
 });
