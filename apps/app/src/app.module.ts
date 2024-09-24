3bd2d027fb835bdec14fd24a2c8504d623df8653
jjong <90lastone@gmail.com>
2024-09-24 11:03:56 +0900

    feat: app get - products

--- apps/app/src/app.module.ts
+++ apps/app/src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthAppModule } from './auth/auth.app.module';
 import { configModule } from '@common/common/setting/config';
+import { ProductsAppModule } from './product/products.app.module';
 
 @Module({
-  imports: [configModule(), AuthAppModule],
+  imports: [configModule(), AuthAppModule, ProductsAppModule],
   controllers: [AppController],
   providers: [AppService],
 })
