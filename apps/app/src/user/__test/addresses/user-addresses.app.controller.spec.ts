cc2ec35a86e8d0f9867d68b1216f967d8563e63b
jongwon-choi <90lastone@gmail.com>
2024-09-26 17:09:22 +0900

    refactor: app userAddresses 자료구조 array -> map 변경

--- apps/app/src/user/__test/addresses/user-addresses.app.controller.spec.ts
+++ apps/app/src/user/__test/addresses/user-addresses.app.controller.spec.ts
@@ -4,7 +4,7 @@ import { ERROR_MESSAGES } from '@common/common/constant/error-messages';
 import { UserAddressesAppController } from '../../addresses/user-addresses.app.controller';
 import { UserAddressesAppService } from '../../addresses/user-addresses.app.service';
 
-describe('UserAppController', () => {
+describe('UserAddressesAppController', () => {
   let userAddressesAppController: UserAddressesAppController;
 
   beforeEach(async () => {
@@ -18,18 +18,35 @@ describe('UserAppController', () => {
     );
   });
 
-  it('post', () => {
-    const userId = 1;
-    const dto = {
-      zipcode: '01235',
-      address: '서울시 강남구 신사동 *********',
-      isDefault: false,
-    };
+  describe('post', () => {
+    it('신규 생성', () => {
+      const userId = 2;
+      const dto = {
+        zipcode: '01235',
+        address: '서울시 강남구 신사동 *********',
+        isDefault: false,
+      };
 
-    expect(userAddressesAppController.postUserAddress(userId, dto)).toEqual({
-      id: 2,
-      userId,
-      ...dto,
+      expect(userAddressesAppController.postUserAddress(userId, dto)).toEqual({
+        userId,
+        id: 1,
+        ...dto,
+      });
+    });
+
+    it('추가 생성', () => {
+      const userId = 1;
+      const dto = {
+        zipcode: '01235',
+        address: '서울시 강남구 신사동 *********',
+        isDefault: false,
+      };
+
+      expect(userAddressesAppController.postUserAddress(userId, dto)).toEqual({
+        userId,
+        id: 2,
+        ...dto,
+      });
     });
   });
 
@@ -38,8 +55,8 @@ describe('UserAppController', () => {
 
     expect(userAddressesAppController.getUserAddresses(userId)).toEqual([
       {
-        id: 1,
         userId,
+        id: 1,
         zipcode: '01234',
         address: '서울시 강남구 역삼동 *********',
         isDefault: true,
@@ -61,27 +78,26 @@ describe('UserAppController', () => {
       expect(
         userAddressesAppController.putUserAddress(userId, id, dto),
       ).toEqual({
-        id,
         userId,
+        id,
         ...dto,
       });
     });
 
     it(ERROR_MESSAGES.UserAddressNotFound, () => {
-      const id = NON_EXISTENT_ID;
-
       expect(() =>
-        userAddressesAppController.putUserAddress(userId, id, dto),
+        userAddressesAppController.putUserAddress(
+          NON_EXISTENT_ID,
+          NON_EXISTENT_ID,
+          dto,
+        ),
       ).toThrow(ERROR_MESSAGES.UserAddressNotFound);
     });
 
-    it(ERROR_MESSAGES.UserAddressForbidden, () => {
-      const id = 1;
-      const userId = 2;
-
+    it(ERROR_MESSAGES.UserAddressNotFound, () => {
       expect(() =>
-        userAddressesAppController.putUserAddress(userId, id, dto),
-      ).toThrow(ERROR_MESSAGES.UserAddressForbidden);
+        userAddressesAppController.putUserAddress(userId, NON_EXISTENT_ID, dto),
+      ).toThrow(ERROR_MESSAGES.UserAddressNotFound);
     });
   });
 
@@ -97,20 +113,9 @@ describe('UserAppController', () => {
     });
 
     it(ERROR_MESSAGES.UserAddressNotFound, () => {
-      const id = NON_EXISTENT_ID;
-
       expect(() =>
-        userAddressesAppController.deleteUserAddress(userId, id),
+        userAddressesAppController.deleteUserAddress(userId, NON_EXISTENT_ID),
       ).toThrow(ERROR_MESSAGES.UserAddressNotFound);
-    });
-
-    it(ERROR_MESSAGES.UserAddressForbidden, () => {
-      const id = 1;
-      const userId = 2;
-
-      expect(() =>
-        userAddressesAppController.deleteUserAddress(userId, id),
-      ).toThrow(ERROR_MESSAGES.UserAddressForbidden);
     });
   });
 });
--- apps/app/src/user/addresses/user-addresses.app.service.ts
+++ apps/app/src/user/addresses/user-addresses.app.service.ts
@@ -1,82 +1,72 @@
-import {
-  ForbiddenException,
-  Injectable,
-  NotFoundException,
-} from '@nestjs/common';
-import { UserAddress } from '@domain/domain/app/user-address';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { UserAddress, UsersAddresses } from '@domain/domain/app/user-address';
 import { ERROR_MESSAGES } from '@common/common/constant/error-messages';
 import { PostUserAddressRequestDto } from './user-addresses.app.dto';
 
 @Injectable()
 export class UserAddressesAppService {
-  /*
-  TODO
-  isDefault 1개만 가능하도록
-  데이터가 있을 경우 isDefault 1개 무조건 유지
-  maxLength 10
-   */
-  #userAddresses: UserAddress[] = [
-    {
-      id: 1,
-      userId: 1,
-      zipcode: '01234',
-      address: '서울시 강남구 역삼동 *********',
-      isDefault: true,
-    },
-  ];
+  #usersAddresses = UsersAddresses.of();
 
   postUserAddress(userId: number, dto: PostUserAddressRequestDto) {
+    /*
+      TODO
+      isDefault 1개만 가능하도록
+      데이터가 있을 경우 isDefault 1개 무조건 유지
+      maxLength 10
+    */
+
+    const userAddresses = this.#usersAddresses.get(userId)
+      ? this.#usersAddresses.get(userId)
+      : new Map<number, UserAddress>();
+
     const userAddress = new UserAddress();
-    userAddress.id = this.#userAddresses.length + 1;
+    userAddress.id =
+      userAddresses.size === 0
+        ? 1
+        : Math.max(...Array.from(userAddresses).map((item) => item[1].id)) + 1;
     userAddress.userId = userId;
     userAddress.zipcode = dto.zipcode;
     userAddress.address = dto.address;
     userAddress.isDefault = dto.isDefault;
 
-    this.#userAddresses.push(userAddress);
+    userAddresses.set(userAddress.id, userAddress);
 
     return userAddress;
   }
 
   getUserAddresses(userId: number) {
-    return this.#userAddresses.filter(
-      (userAddress) => userAddress.userId === userId,
-    );
+    return Array.from(this.#usersAddresses.get(userId)).map((item) => item[1]);
   }
 
   putUserAddress(dto: UserAddress) {
-    const userAddress = this.#userAddresses.find(
-      (userAddress) => userAddress.id === dto.id,
-    );
+    const userAddresses = this.#usersAddresses.get(dto.userId);
 
-    if (!userAddress) {
+    if (!userAddresses) {
       throw new NotFoundException(ERROR_MESSAGES.UserAddressNotFound);
     }
 
-    if (userAddress.userId !== dto.userId) {
-      throw new ForbiddenException(ERROR_MESSAGES.UserAddressForbidden);
+    const userAddress = userAddresses.get(dto.id);
+
+    if (!userAddress) {
+      throw new NotFoundException(ERROR_MESSAGES.UserAddressNotFound);
     }
 
     userAddress.zipcode = dto.zipcode;
     userAddress.address = dto.address;
     userAddress.isDefault = dto.isDefault;
 
+    userAddresses.set(userAddress.id, userAddress);
+
     return userAddress;
   }
 
   deleteUserAddress(userId: number, id: number) {
-    const userAddressIndex = this.#userAddresses.findIndex(
-      (userAddress) => userAddress.id === id,
-    );
+    const userAddresses = this.#usersAddresses.get(userId);
 
-    if (userAddressIndex === -1) {
+    if (!userAddresses.has(id)) {
       throw new NotFoundException(ERROR_MESSAGES.UserAddressNotFound);
     }
 
-    if (this.#userAddresses[userAddressIndex].userId !== userId) {
-      throw new ForbiddenException(ERROR_MESSAGES.UserAddressForbidden);
-    }
-
-    this.#userAddresses.splice(userAddressIndex, 1);
+    userAddresses.delete(id);
   }
 }
