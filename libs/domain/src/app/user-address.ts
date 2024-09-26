3b2341d77de151380c9b91c07dfd307497d88206
jongwon-choi <90lastone@gmail.com>
2024-09-26 17:09:22 +0900

    refactor: app userAddresses 자료구조 array -> map 변경

--- libs/domain/src/app/user-address.ts
+++ libs/domain/src/app/user-address.ts
@@ -2,8 +2,8 @@ import { ApiProperty } from '@nestjs/swagger';
 import { IsBoolean, IsNotEmpty, IsString } from 'class-validator';
 
 export class UserAddress {
-  id: number;
   userId: number;
+  id: number;
   @ApiProperty()
   @IsNotEmpty()
   @IsString()
@@ -16,4 +16,21 @@ export class UserAddress {
   @IsNotEmpty()
   @IsBoolean()
   isDefault: boolean;
+}
+
+export class UsersAddresses {
+  static of() {
+    const usersAddresses = new Map<number, Map<number, UserAddress>>();
+
+    const userAddress = new Map<number, UserAddress>();
+    userAddress.set(1, {
+      userId: 1,
+      id: 1,
+      zipcode: '01234',
+      address: '서울시 강남구 역삼동 *********',
+      isDefault: true,
+    });
+
+    return usersAddresses.set(1, userAddress);
+  }
 }
