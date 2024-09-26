e8029f6b4b2d683170c0ece08d220b4fe653e1d8
jongwon-choi <90lastone@gmail.com>
2024-09-26 12:31:53 +0900

    test: app 사용자 주소 수정

--- apps/app/src/user/__test/user.app.controller.spec.ts
+++ apps/app/src/user/__test/user.app.controller.spec.ts
@@ -1,6 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UserAppController } from '../user.app.controller';
 import { UserAppService } from '../user.app.service';
+import { NON_EXISTENT_ID } from '@common/common/constant/constants';
 
 describe('UserAppController', () => {
   let userAppController: UserAppController;
@@ -26,6 +27,42 @@ describe('UserAppController', () => {
       id: 2,
       userId,
       ...dto,
+    });
+  });
+
+  describe('put', () => {
+    const userId = 1;
+    const dto = {
+      zipcode: '55555',
+      address: '서울시 강남구 양재동 *********',
+      isDefault: false,
+    };
+
+    it('성공', () => {
+      const id = 1;
+
+      expect(userAppController.putUserAddress(userId, id, dto)).toEqual({
+        id,
+        userId,
+        ...dto,
+      });
+    });
+
+    it('User address not found', () => {
+      const id = NON_EXISTENT_ID;
+
+      expect(() => userAppController.putUserAddress(userId, id, dto)).toThrow(
+        'User address not found',
+      );
+    });
+
+    it('User address forbidden', () => {
+      const id = 1;
+      const userId = 2;
+
+      expect(() => userAppController.putUserAddress(userId, id, dto)).toThrow(
+        'User address forbidden',
+      );
     });
   });
 });
