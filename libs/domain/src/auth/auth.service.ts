e3e280aff6783fbcca9ab79ee7a45b8bc72a8441
jongwon-choi <90lastone@gmail.com>
2024-09-27 18:08:36 +0900

    refactor: jwt payload createToken 내에서 만들도록 개선

--- libs/domain/src/auth/auth.service.ts
+++ libs/domain/src/auth/auth.service.ts
@@ -1,14 +1,18 @@
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import * as jwt from 'jsonwebtoken';
+import { AppUser } from '@domain/domain/app/user';
+import { AdminUser } from '@domain/domain/admin/user';
 
 @Injectable()
 export class AuthService {
   constructor(private readonly configService: ConfigService) {}
 
-  createToken(payload: any) {
+  createToken(user: AdminUser | AppUser) {
     const secret = this.configService.get<string>('JWT_SECRET');
     const expiresIn = this.configService.get<string>('JWT_EXPIRES_IN');
+
+    const payload = { sub: user.id, email: user.email, name: user.name };
 
     return jwt.sign(payload, secret, {
       expiresIn,
