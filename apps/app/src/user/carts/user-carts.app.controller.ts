d6cb6ee50a02172757fdecdfb17b7c9eb023acf0
jjong <akrTmwk@gmail.com>
2024-09-29 15:03:07 +0900

    feat: app user carts post

--- apps/app/src/user/carts/user-carts.app.controller.ts
+++ apps/app/src/user/carts/user-carts.app.controller.ts
@@ -1,9 +1,20 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post, Version } from '@nestjs/common';
 import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
+import { UserCartsAppService } from './user-carts.app.service';
+import { Public } from '@common/common/decorator/public.decorator';
+import { PostUserCartsAppReqDto } from './user-carts.app.dto';
+import { UserId } from '@common/common/decorator/user-id.decorator';
 
 @ApiBearerAuth('jwt')
 @ApiTags('user')
 @Controller('user/carts')
 export class UserCartsAppController {
-  constructor() {}
+  constructor(private readonly userCartsAppService: UserCartsAppService) {}
+
+  @Public()
+  @Version('1')
+  @Post()
+  postUserCart(@UserId() userId: number, @Body() dto: PostUserCartsAppReqDto) {
+    return this.userCartsAppService.postUserCart(userId, dto);
+  }
 }
