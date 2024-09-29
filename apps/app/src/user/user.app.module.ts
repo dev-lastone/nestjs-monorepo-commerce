3d9560f5a9ad9fc06667c47357d791b74bed26f8
jjong <akrTmwk@gmail.com>
2024-09-29 15:03:07 +0900

    feat: app user carts post

--- apps/app/src/user/user.app.module.ts
+++ apps/app/src/user/user.app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { UserAppController } from './user.app.controller';
 import { UserAppService } from './user.app.service';
 import { UserAddressesAppModule } from './addresses/user-addresses.app.module';
+import { UserCartsAppModule } from './carts/user-carts.app.module';
 
 @Module({
-  imports: [UserAddressesAppModule],
+  imports: [UserAddressesAppModule, UserCartsAppModule],
   controllers: [UserAppController],
   providers: [UserAppService],
 })
