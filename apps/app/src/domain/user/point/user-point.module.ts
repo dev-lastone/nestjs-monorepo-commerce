f98dc10a5dd2b4a1025c7db298baf4086ff6a9f9
jjong <akrTmwk@gmail.com>
2024-10-02 22:39:01 +0900

    refactor: UserPointModule 미사용 임시 제거

--- apps/app/src/domain/user/point/user-point.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common';
-import { UserPointService } from './user-point.service';
-
-@Module({
-  providers: [UserPointService],
-  exports: [UserPointService],
-})
-export class UserPointModule {}
