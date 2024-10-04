abc3fe73f0fac10970959cfea2e1a90583f7bf59
jongwon-choi <90lastone@gmail.com>
2024-10-04 14:18:09 +0900

    docs: user point 구성 메모

--- apps/app/src/domain/user/point/user-point.ts
+++ apps/app/src/domain/user/point/user-point.ts
@@ -1,5 +1,33 @@
 import { ApiProperty } from '@nestjs/swagger';
 
+/*
+	적립
+	- 구매확정
+	- 리뷰확정
+	- 수동 (관리자)
+
+	사용
+	- 구매
+
+	복구
+	- 구매 취소 (만료일 지난 경우 취소일 기준 자정)
+
+	UserPoint (유저 총 포인트 관리) - 포인트 분리. user row lock 방지
+	- userId
+	- point
+
+	UserPointAction (포인트 적립/사용/복구 이력)
+	- userPointId
+	- id
+	- action (구매확정, 리뷰확정, 수동 admin, 구매, 구매취소)
+	- actionId
+	- point
+	- details ([id: 1, point: 100])
+	- createdAt
+	- updatedAt
+	- expirationAt
+ */
+
 export enum UserPointActionType {
   ORDER_PRODUCT = 'order-product',
   REVIEW = 'review',
