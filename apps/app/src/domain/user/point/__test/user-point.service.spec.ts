10f1c6337bd04480deb61289f43a8e230f09d801
jongwon-choi <90lastone@gmail.com>
2024-10-04 18:04:40 +0900

    test: user point remainingPoint 추가

--- apps/app/src/domain/user/point/__test/user-point.service.spec.ts
+++ apps/app/src/domain/user/point/__test/user-point.service.spec.ts
@@ -4,49 +4,54 @@ import { UserPointHistoryAction } from '../user-point';
 describe('UserPointService', () => {
   const userPointService = new UserPointService();
 
-  it('save', () => {
-    const userPoint = userPointService.savePoint(
-      1,
-      1000,
-      UserPointHistoryAction.ORDER_PRODUCT,
-      1,
-    );
-    expect(userPoint).toEqual({
-      userId: 1,
-      id: 2,
-      point: 1000,
-      action: UserPointHistoryAction.ORDER_PRODUCT,
-      actionId: 1,
+  describe('save', () => {
+    it('성공', () => {
+      const userPoint = userPointService.savePoint(
+        1,
+        1000,
+        UserPointHistoryAction.ORDER_PRODUCT,
+        3,
+      );
+      expect(userPoint).toEqual({
+        userId: 1,
+        id: 3,
+        point: 1000,
+        remainingPoint: 1000,
+        action: UserPointHistoryAction.ORDER_PRODUCT,
+        actionId: 3,
+      });
     });
-  });
 
-  it('save - 신규 생성', () => {
-    const userPoint = userPointService.savePoint(
-      2,
-      1000,
-      UserPointHistoryAction.ORDER_PRODUCT,
-      1,
-    );
-    expect(userPoint).toEqual({
-      userId: 2,
-      id: 3,
-      point: 1000,
-      action: UserPointHistoryAction.ORDER_PRODUCT,
-      actionId: 1,
+    it('성공 - 신규 생성', () => {
+      const userPoint = userPointService.savePoint(
+        2,
+        1000,
+        UserPointHistoryAction.ORDER_PRODUCT,
+        4,
+      );
+      expect(userPoint).toEqual({
+        userId: 2,
+        id: 4,
+        point: 1000,
+        remainingPoint: 1000,
+        action: UserPointHistoryAction.ORDER_PRODUCT,
+        actionId: 4,
+      });
     });
   });
 
   it('use', () => {
     const userPoint = userPointService.usePoint(
       1,
-      1000,
+      1500,
       UserPointHistoryAction.ORDER,
       1,
     );
     expect(userPoint).toEqual({
       userId: 1,
-      id: 4,
-      point: 1000,
+      id: 5,
+      point: 1500,
+      remainingPoint: 0,
       action: UserPointHistoryAction.ORDER,
       actionId: 1,
     });
