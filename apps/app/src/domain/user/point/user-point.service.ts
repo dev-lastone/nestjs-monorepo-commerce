fb2d7d62fe619b82bd7025bcc146716f19b9c776
jongwon-choi <90lastone@gmail.com>
2024-10-04 15:03:57 +0900

    refactor: user point 이력 추적가능하도록 구조 개선

--- apps/app/src/domain/user/point/user-point.service.ts
+++ apps/app/src/domain/user/point/user-point.service.ts
@@ -1,49 +1,76 @@
 import { Injectable } from '@nestjs/common';
-import { UserPoint, UserPointActionType } from './user-point';
+import {
+  UserPoint,
+  UserPointHistory,
+  UserPointHistoryAction,
+} from './user-point';
 
 @Injectable()
 export class UserPointService {
   #userPoints: UserPoint[] = [
     {
       userId: 1,
-      id: 1,
       point: 1000,
-      actionType: UserPointActionType.ORDER_PRODUCT,
+    },
+  ];
+
+  #userPointHistories: UserPointHistory[] = [
+    {
+      userId: 1,
+      id: 1,
+      action: UserPointHistoryAction.ORDER_PRODUCT,
       actionId: 1,
+      point: 1000,
     },
   ];
 
   savePoint(
     userId: number,
     point: number,
-    actionType: UserPointActionType,
+    action: UserPointHistoryAction,
     actionId: number,
   ): UserPoint {
-    const userPoint: UserPoint = {
-      userId,
-      id: this.#userPoints.length + 1,
-      point,
-      actionType,
-      actionId,
-    };
+    const userPoint =
+      this.#userPoints.find((userPoint) => userPoint.userId === userId) ??
+      new UserPoint();
+
+    userPoint.point += point;
+
+    const userPointHistory = new UserPointHistory();
+    userPointHistory.userId = userId;
+    userPointHistory.id = this.#userPointHistories.length + 1;
+    userPointHistory.action = action;
+    userPointHistory.actionId = actionId;
+    userPointHistory.point = point;
+
     this.#userPoints.push(userPoint);
-    return userPoint;
+    this.#userPointHistories.push(userPointHistory);
+
+    return userPointHistory;
   }
-  
+
   usePoint(
     userId: number,
     point: number,
-    actionType: UserPointActionType,
+    action: UserPointHistoryAction,
     actionId: number,
   ): UserPoint {
-    const userPoint: UserPoint = {
-      userId,
-      id: this.#userPoints.length + 1,
-      point: -point,
-      actionType,
-      actionId,
-    };
+    const userPoint = this.#userPoints.find(
+      (userPoint) => userPoint.userId === userId,
+    );
+
+    userPoint.point -= point;
+
+    const userPointHistory = new UserPointHistory();
+    userPointHistory.userId = userId;
+    userPointHistory.id = this.#userPointHistories.length + 1;
+    userPointHistory.action = action;
+    userPointHistory.actionId = actionId;
+    userPointHistory.point = point;
+
     this.#userPoints.push(userPoint);
-    return userPoint;
+    this.#userPointHistories.push(userPointHistory);
+
+    return userPointHistory;
   }
 }
