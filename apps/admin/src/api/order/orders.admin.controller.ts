831a605327d19081a1ba826aa074e99333f24f51
jongwon-choi <90lastone@gmail.com>
2024-10-07 17:55:22 +0900

    feat: admin get order

--- apps/admin/src/api/order/orders.admin.controller.ts
+++ apps/admin/src/api/order/orders.admin.controller.ts
@@ -1,13 +1,14 @@
-import { Controller, Get, Version } from '@nestjs/common';
+import { Controller, Get, Param, ParseIntPipe, Version } from '@nestjs/common';
 import { ApiBearerAuth, ApiOkResponse, ApiTags } from '@nestjs/swagger';
 import { orders } from '@domain/domain/order/orders';
 import { Order } from '@domain/domain/order/order';
+import { OrdersAdminService } from './orders.admin.service';
 
 @ApiBearerAuth('jwt')
 @ApiTags('orders')
 @Controller('orders')
 export class OrdersAdminController {
-  constructor() {}
+  constructor(private readonly ordersAdminService: OrdersAdminService) {}
 
   @Version('1')
   @Get()
@@ -19,5 +20,12 @@ export class OrdersAdminController {
     return orders;
   }
 
-  // TODO 주문 상태 변경
+  @Version('1')
+  @Get(':id')
+  @ApiOkResponse({
+    type: Order,
+  })
+  getOrder(@Param('id', new ParseIntPipe()) id: number) {
+    return this.ordersAdminService.getOrder(id);
+  }
 }
