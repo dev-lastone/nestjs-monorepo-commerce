60d8e192a4c4e5a3fdff9f3344b43470e749f87c
jongwon-choi <90lastone@gmail.com>
2024-10-07 17:55:25 +0900

    test: admin get order

--- apps/admin/src/api/order/__test/orders.admin.controller.spec.ts
+++ apps/admin/src/api/order/__test/orders.admin.controller.spec.ts
@@ -1,6 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { OrdersAdminController } from '../orders.admin.controller';
 import { orders } from '@domain/domain/order/orders';
+import { OrdersAdminService } from '../orders.admin.service';
 
 describe('OrdersAdminController', () => {
   let ordersAdminController: OrdersAdminController;
@@ -8,6 +9,7 @@ describe('OrdersAdminController', () => {
   beforeEach(async () => {
     const app: TestingModule = await Test.createTestingModule({
       controllers: [OrdersAdminController],
+      providers: [OrdersAdminService],
     }).compile();
 
     ordersAdminController = app.get<OrdersAdminController>(
@@ -15,7 +17,12 @@ describe('OrdersAdminController', () => {
     );
   });
 
-  it('get', () => {
+  it('getOrders', () => {
     expect(ordersAdminController.getOrders()).toEqual(orders);
+  });
+
+  it('getOrder', () => {
+    const id = 1;
+    expect(ordersAdminController.getOrder(id)).toEqual(orders[0]);
   });
 });
