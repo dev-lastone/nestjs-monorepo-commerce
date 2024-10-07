f59022dc5e5e967e43d2d13b8122833fe6d2e10e
jongwon-choi <90lastone@gmail.com>
2024-10-07 18:50:02 +0900

    refactor: admin get orders. products 제외되도록

--- libs/domain/src/order/order.ts
+++ libs/domain/src/order/order.ts
@@ -1,25 +1,31 @@
 import { OrderProduct } from './order-product';
 import { ApiProperty } from '@nestjs/swagger';
+import { Expose } from 'class-transformer';
 
 export class Order {
   @ApiProperty({
     example: 1,
   })
+  @Expose()
   id: number;
   @ApiProperty({
     example: 1,
   })
+  @Expose()
   userId: number;
   @ApiProperty({
     example: '01234',
   })
+  @Expose()
   zipcode: string;
   @ApiProperty({
     example: '서울시 강남구 역삼동 *********',
   })
+  @Expose()
   address: string;
   @ApiProperty({
     type: [OrderProduct],
   })
+  @Expose()
   products: OrderProduct[];
 }
