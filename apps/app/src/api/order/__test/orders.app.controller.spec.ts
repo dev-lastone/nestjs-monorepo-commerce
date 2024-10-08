a8b877c2a483b64e54190de7889c0bd8364ed45c
jongwon-choi <90lastone@gmail.com>
2024-10-08 17:48:46 +0900

    test: orders -> OrderRepo 구현

--- apps/app/src/api/order/__test/orders.app.controller.spec.ts
+++ apps/app/src/api/order/__test/orders.app.controller.spec.ts
@@ -7,13 +7,14 @@ import {
   productStub2,
 } from '@domain/domain/product/__stub/product.stub';
 import { OrderStatus } from '@domain/domain/order/order-product';
+import { OrderModule } from '@domain/domain/order/order.module';
 
 describe('OrdersAppController', () => {
   let ordersAppController: OrdersAppController;
 
   beforeEach(async () => {
     const app: TestingModule = await Test.createTestingModule({
-      imports: [ProductModule],
+      imports: [ProductModule, OrderModule],
       controllers: [OrdersAppController],
       providers: [OrdersAppService],
     }).compile();
@@ -27,21 +28,21 @@ describe('OrdersAppController', () => {
         productIds: [1, 2],
       }),
     ).toEqual({
-      id: 2,
+      id: 1,
       userId: 1,
       zipcode: '01234',
       address: '서울시 강남구 역삼동 *********',
       products: [
         {
-          orderId: 2,
           id: 1,
+          orderId: 1,
           productId: productStub1.id,
           name: productStub1.name,
           price: productStub1.price,
           status: OrderStatus.ORDERED,
         },
         {
-          orderId: 2,
+          orderId: 1,
           id: 2,
           productId: productStub2.id,
           name: productStub2.name,
@@ -53,6 +54,10 @@ describe('OrdersAppController', () => {
   });
 
   it('get', () => {
+    ordersAppController.postOrder(1, {
+      productIds: [1, 2],
+    });
+
     expect(ordersAppController.getOrders(1)).toEqual([
       {
         id: 1,
@@ -61,32 +66,16 @@ describe('OrdersAppController', () => {
         address: '서울시 강남구 역삼동 *********',
         products: [
           {
+            id: 1,
             orderId: 1,
-            id: 1,
-            productId: productStub1.id,
-            name: productStub1.name,
-            price: productStub1.price,
-            status: OrderStatus.ORDERED,
-          },
-        ],
-      },
-      {
-        id: 2,
-        userId: 1,
-        zipcode: '01234',
-        address: '서울시 강남구 역삼동 *********',
-        products: [
-          {
-            orderId: 2,
-            id: 1,
             productId: productStub1.id,
             name: productStub1.name,
             price: productStub1.price,
             status: OrderStatus.ORDERED,
           },
           {
-            orderId: 2,
             id: 2,
+            orderId: 1,
             productId: productStub2.id,
             name: productStub2.name,
             price: productStub2.price,
