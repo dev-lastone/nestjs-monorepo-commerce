e95c4c32583fde21b184c0a2fad2ceef82bc6d4f
jjong <akrTmwk@gmail.com>
2024-10-09 22:28:09 +0900

    refactor: ProductRepo 적용

--- libs/domain/src/product/product.module.ts
+++ libs/domain/src/product/product.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { ProductService } from './product.service';
+import { ProductRepo } from '@domain/domain/product/product.repo';
 
 @Module({
-  providers: [ProductService],
+  providers: [ProductService, ProductRepo],
   exports: [ProductService],
 })
 export class ProductModule {}
