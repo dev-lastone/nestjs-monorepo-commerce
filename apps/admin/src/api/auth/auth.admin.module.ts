387bbfe02da9f0273686f363cf928aa8a890e182
jjong <akrTmwk@gmail.com>
2024-10-09 22:06:25 +0900

    refactor: AdminUserRepo 적용

--- apps/admin/src/api/auth/auth.admin.module.ts
+++ apps/admin/src/api/auth/auth.admin.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AuthAdminService } from './auth.admin.service';
 import { AuthAdminController } from './auth.admin.controller';
 import { AuthModule } from '@domain/domain/auth/auth.module';
+import { AdminUserModule } from '@domain/domain/admin-user/admin-user.module';
 
 @Module({
-  imports: [AuthModule],
+  imports: [AuthModule, AdminUserModule],
   controllers: [AuthAdminController],
   providers: [AuthAdminService],
 })
