4b765cc032f0575869fdabde1fa37a5cf6538290
jjong <akrTmwk@gmail.com>
2024-10-09 17:07:00 +0900

    refactor: userCartStub

--- apps/app/src/api/user/__test/carts/user-carts.app.controller.spec.ts
+++ apps/app/src/api/user/__test/carts/user-carts.app.controller.spec.ts
@@ -2,6 +2,9 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { UserCartsAppController } from '../../carts/user-carts.app.controller';
 import { ERROR_MESSAGES } from '@common/common/constant/error-messages';
 import { UserCartService } from '../../../../domain/user/cart/user-cart.service';
+import { productStub1 } from '@domain/domain/product/__stub/product.stub';
+import { appUserStub } from '@domain/domain/user/__stub/app-user.stub';
+import { userCartStub } from '../../../../domain/user/cart/__stub/user-cart.stub';
 
 describe('UserCartsAppController', () => {
   let userCartsAppController: UserCartsAppController;
@@ -18,46 +21,52 @@ describe('UserCartsAppController', () => {
   });
 
   it('post', () => {
-    const dto = { productId: 1, count: 1 };
-    expect(userCartsAppController.postUserCart(1, dto)).toEqual({
+    const dto = { productId: productStub1.id, count: 1 };
+    expect(userCartsAppController.postUserCart(appUserStub.id, dto)).toEqual({
       id: 2,
-      userId: 1,
-      productId: 1,
-      count: 1,
+      userId: appUserStub.id,
+      productId: dto.productId,
+      count: dto.count,
     });
   });
 
   it('get', () => {
-    expect(userCartsAppController.getUserCarts(1)).toEqual([
-      {
-        id: 1,
-        userId: 1,
-        productId: 1,
-        count: 1,
-      },
+    expect(userCartsAppController.getUserCarts(appUserStub.id)).toEqual([
+      userCartStub,
     ]);
   });
 
   describe('put', () => {
     it(ERROR_MESSAGES.UserCartNotFound, () => {
       const dto = { count: 2 };
-      expect(() => userCartsAppController.putUserCart(1, 2, dto)).toThrowError(
-        ERROR_MESSAGES.UserCartNotFound,
-      );
+      expect(() =>
+        userCartsAppController.putUserCart(appUserStub.id, 2, dto),
+      ).toThrowError(ERROR_MESSAGES.UserCartNotFound);
     });
 
     it('성공', () => {
       const dto = { count: 2 };
-      expect(userCartsAppController.putUserCart(1, 1, dto)).toEqual({
-        id: 1,
-        userId: 1,
-        productId: 1,
-        count: 2,
+      expect(
+        userCartsAppController.putUserCart(
+          userCartStub.userId,
+          userCartStub.id,
+          dto,
+        ),
+      ).toEqual({
+        id: userCartStub.id,
+        userId: userCartStub.userId,
+        productId: userCartStub.productId,
+        count: dto.count,
       });
     });
   });
 
   it('delete', () => {
-    expect(userCartsAppController.deleteUserCart(1, 1)).toBeUndefined();
+    expect(
+      userCartsAppController.deleteUserCart(
+        userCartStub.userId,
+        userCartStub.id,
+      ),
+    ).toBeUndefined();
   });
 });
