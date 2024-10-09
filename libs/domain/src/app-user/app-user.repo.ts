a5fd09df06658df16781b2bbc6bfaba847bde9d6
jjong <akrTmwk@gmail.com>
2024-10-09 21:57:50 +0900

    feat: AppUserRepo save, findOne

--- libs/domain/src/app-user/app-user.repo.ts
+++ libs/domain/src/app-user/app-user.repo.ts
@@ -1,4 +1,18 @@
 import { Injectable } from '@nestjs/common';
+import { AppUser } from '@domain/domain/app-user/app-user';
+import { appUserStub } from '@domain/domain/user/__stub/app-user.stub';
 
 @Injectable()
-export class AppUserRepo {}
+export class AppUserRepo {
+  #appUsers: AppUser[] = [appUserStub];
+
+  save(appUser: AppUser) {
+    appUser.id = this.#appUsers[this.#appUsers.length - 1].id + 1;
+    this.#appUsers.push(appUser);
+    return appUser;
+  }
+
+  findOne(appUser: Partial<AppUser>) {
+    return this.#appUsers.find((user) => user.email === appUser.email);
+  }
+}
