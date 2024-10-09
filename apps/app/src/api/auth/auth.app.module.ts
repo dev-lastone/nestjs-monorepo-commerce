d0cf2b82e1e5863543453dd76645e5057164e1f3
jjong <akrTmwk@gmail.com>
2024-10-09 21:58:05 +0900

    feat: AuthAppService AppUserRepo 적용

--- apps/app/src/api/auth/auth.app.module.ts
+++ apps/app/src/api/auth/auth.app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AuthAppService } from './auth.app.service';
 import { AuthAppController } from './auth.app.controller';
 import { AuthModule } from '@domain/domain/auth/auth.module';
+import { AppUserModule } from '@domain/domain/app-user/app-user.module';
 
 @Module({
-  imports: [AuthModule],
+  imports: [AuthModule, AppUserModule],
   controllers: [AuthAppController],
   providers: [AuthAppService],
 })
