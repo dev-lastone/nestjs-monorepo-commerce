27a6a41f656ecdf4fb25777444785659dde8fc69
jjong <akrTmwk@gmail.com>
2024-10-11 12:05:36 +0900

    fix: OrderProductsAdminModule import OrderModule

--- apps/admin/src/api/order/order-products/order-products.admin.module.ts
+++ apps/admin/src/api/order/order-products/order-products.admin.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { OrderProductsAdminController } from './order-products.admin.controller';
 import { OrderProductsAdminService } from './order-products.admin.service';
+import { OrderModule } from '@domain/domain/order/order.module';
 
 @Module({
+  imports: [OrderModule],
   controllers: [OrderProductsAdminController],
   providers: [OrderProductsAdminService],
 })
