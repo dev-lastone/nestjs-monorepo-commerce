b93a6ef370f6f9a48a7d245da0cd5d9e2b1e2e6f
jjong <akrTmwk@gmail.com>
2024-10-11 12:25:43 +0900

    fix: orderStub products id 추가

--- libs/domain/src/order/__stub/order.stub.ts
+++ libs/domain/src/order/__stub/order.stub.ts
@@ -2,7 +2,10 @@ import { productStub1 } from '@domain/domain/product/__stub/product.stub';
 import { Order } from '@domain/domain/order/order';
 import { userAddressStub } from '../../../../../apps/app/src/domain/user/address/__stub/user-address.stub';
 
+const order = new Order(userAddressStub, [productStub1]);
+order.products[0].id = 1;
+
 export const orderStub = {
   id: 1,
-  ...new Order(userAddressStub, [productStub1]),
+  ...order,
 };
