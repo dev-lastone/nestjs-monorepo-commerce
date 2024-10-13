cf19f6251e72b0f6cca33289c3a26e14ac7deb55
jjong <akrTmwk@gmail.com>
2024-10-13 21:18:27 +0900

    test: OrderProductsAdminController patchOrderProduct 의존성만 테스트하도록 개선

--- apps/admin/src/api/order/__test/order-products.admin.controller.spec.ts
+++ apps/admin/src/api/order/__test/order-products.admin.controller.spec.ts
@@ -2,35 +2,40 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { OrderProductsAdminService } from '../order-products/order-products.admin.service';
 import { OrderProductsAdminController } from '../order-products/order-products.admin.controller';
 import { OrderProductStatus } from '@domain/domain/order/order-product';
-import { productStub1 } from '@domain/domain/product/__stub/product.stub';
-import { OrderModule } from '@domain/domain/order/order.module';
 
 describe('OrderProductsAdminController', () => {
   let orderProductsAdminController: OrderProductsAdminController;
+  let orderProductsAdminService: OrderProductsAdminService;
 
   beforeEach(async () => {
     const app: TestingModule = await Test.createTestingModule({
-      imports: [OrderModule],
       controllers: [OrderProductsAdminController],
-      providers: [OrderProductsAdminService],
+      providers: [
+        {
+          provide: OrderProductsAdminService,
+          useValue: {
+            patchOrderProduct: jest.fn(),
+          },
+        },
+      ],
     }).compile();
 
     orderProductsAdminController = app.get<OrderProductsAdminController>(
       OrderProductsAdminController,
     );
+    orderProductsAdminService = app.get<OrderProductsAdminService>(
+      OrderProductsAdminService,
+    );
   });
 
   it('patchOrderProduct', () => {
-    expect(
-      orderProductsAdminController.patchOrderProduct(1, {
-        status: OrderProductStatus.ON_DELIVERY,
-      }),
-    ).toEqual({
-      id: 1,
-      productId: productStub1.id,
-      name: productStub1.name,
-      price: productStub1.price,
+    const id = 1;
+    const dto = {
       status: OrderProductStatus.ON_DELIVERY,
-    });
+    };
+
+    orderProductsAdminController.patchOrderProduct(id, dto);
+
+    expect(orderProductsAdminService.patchOrderProduct).toBeCalledWith(id, dto);
   });
 });
