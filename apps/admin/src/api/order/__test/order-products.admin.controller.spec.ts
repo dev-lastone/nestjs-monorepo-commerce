b9410fc6bc33a401dca2fe7e7193e16b350e055a
jjong <akrTmwk@gmail.com>
2024-10-13 21:38:54 +0900

    fix: OrderProductsAdminController 테스트 코드 누락 재추가

--- apps/admin/src/api/order/__test/order-products.admin.controller.spec.ts
+++ apps/admin/src/api/order/__test/order-products.admin.controller.spec.ts
@@ -2,15 +2,24 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { OrderProductsAdminService } from '../order-products/order-products.admin.service';
 import { OrderProductsAdminController } from '../order-products/order-products.admin.controller';
 import { OrderProductStatus } from '@domain/domain/order/order-product';
+import { OrderModule } from '@domain/domain/order/order.module';
 
 describe('OrderProductsAdminController', () => {
   let orderProductsAdminController: OrderProductsAdminController;
+  let orderProductsAdminService: OrderProductsAdminService;
 
   beforeEach(async () => {
     const app: TestingModule = await Test.createTestingModule({
       imports: [OrderModule],
       controllers: [OrderProductsAdminController],
-      providers: [OrderProductsAdminService],
+      providers: [
+        {
+          provide: OrderProductsAdminService,
+          useValue: {
+            patchOrderProduct: jest.fn(),
+          },
+        },
+      ],
     }).compile();
 
     orderProductsAdminController = app.get<OrderProductsAdminController>(
