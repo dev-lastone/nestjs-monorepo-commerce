50ca349113be04ea9c0a689f76a2f71d26d75db0
jongwon-choi <90lastone@gmail.com>
2024-10-16 12:23:49 +0900

    feat: app order-products patch api

--- apps/app/src/api/app.module.ts
+++ apps/app/src/api/app.module.ts
@@ -9,12 +9,14 @@ import { JwtAuthGuard } from '@common/common/guard/jwt.auth.guard';
 import { ProductLikeAppModule } from './product/like/product-like.app.module';
 import { UserAppModule } from './user/user.app.module';
 import { OrdersAppModule } from './order/orders.app.module';
+import { OrderProductsAppModule } from './order/products/order-products.app.module';
 
 @Module({
   imports: [
     configModule(),
     AuthAppModule,
     OrdersAppModule,
+    OrderProductsAppModule,
     UserAppModule,
     ProductsAppModule,
     ProductLikeAppModule,
