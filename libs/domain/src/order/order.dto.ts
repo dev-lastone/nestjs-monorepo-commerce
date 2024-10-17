00317c105d52ff434951f552b0772ee1ebc590df
jongwon-choi <90lastone@gmail.com>
2024-10-17 12:50:08 +0900

    refactor: patchOrderProduct -> deliver 별도 분리

--- libs/domain/src/order/order.dto.ts
+++
@@ -1,4 +0,0 @@
-import { OrderProduct } from '@domain/domain/order/order-product';
-import { PickType } from '@nestjs/swagger';
-
-export class PatchOrderProductDto extends PickType(OrderProduct, ['status']) {}
