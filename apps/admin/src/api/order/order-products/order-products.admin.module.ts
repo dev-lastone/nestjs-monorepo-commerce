6b8900bc58852a7539dbd0ab945bd4e2d396392a
jongwon-choi <90lastone@gmail.com>
2024-10-18 17:16:49 +0900

    refactor: @application/application -> @application 의미없는 경로 제거

--- apps/admin/src/api/order/order-products/order-products.admin.module.ts
+++ apps/admin/src/api/order/order-products/order-products.admin.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
 import { OrderProductsAdminController } from './order-products.admin.controller';
-import { OrderApplicationModule } from '@application/application/order/order.application.module';
+import { OrderApplicationModule } from '@application/order/order.application.module';
 
 @Module({
   imports: [OrderApplicationModule],
