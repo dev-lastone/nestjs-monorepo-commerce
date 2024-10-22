f0eec243901d8fcf33eaeffdf64579faceb5c6b4
jongwon-choi <90lastone@gmail.com>
2024-10-22 19:13:09 +0900

    refactor: Product Repo 적용

--- libs/domain/src/product/product.module.ts
+++ libs/domain/src/product/product.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ProductRepo } from '@domain/product/product.repo';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Product } from '@domain/product/product.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Product])],
   providers: [ProductRepo],
   exports: [ProductRepo],
 })
