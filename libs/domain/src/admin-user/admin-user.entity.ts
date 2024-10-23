5e70c770a76aac5a08c8d2b3c26cd97ab7bf29f2
jongwon-choi <90lastone@gmail.com>
2024-10-23 12:38:14 +0900

    feat: AdminUser entity column 정의

--- libs/domain/src/admin-user/admin-user.entity.ts
+++ libs/domain/src/admin-user/admin-user.entity.ts
@@ -1,18 +1,21 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { IsEmail, IsNotEmpty } from 'class-validator';
-import { Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
 // TODO admin jwt 별도 발급시 admin domain 으로 이동 예정
 @Entity('user', { schema: 'admin' })
 export class AdminUser {
   @PrimaryGeneratedColumn()
   id: number;
+  @Column({ name: 'name', type: 'varchar', length: 10 })
   name: string;
   @ApiProperty({ default: 'test@test.com' })
   @IsEmail()
   @IsNotEmpty()
+  @Column({ name: 'email', type: 'varchar', length: 100, unique: true })
   email: string;
   @ApiProperty({ default: '1234' })
   @IsNotEmpty()
+  @Column({ name: 'password', type: 'varchar', length: 20 })
   password: string;
 }
