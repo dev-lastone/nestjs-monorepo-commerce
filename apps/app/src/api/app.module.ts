d82c8875c4d7f572e5ca1e8512278148aae99e9f
jjong <akrTmwk@gmail.com>
2024-10-23 23:38:44 +0900

    refactor: AppModule typeorm entities 각 객체 -> path

--- apps/app/src/api/app.module.ts
+++ apps/app/src/api/app.module.ts
@@ -10,12 +10,7 @@ import { ProductLikeAppModule } from './product/like/product-like.app.module';
 import { UserAppModule } from './user/user.app.module';
 import { OrderAppModule } from './order/order.app.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { AppUser } from '@domain/app-user/app-user.entity';
-import { AppUserPointHistory } from '@domain/app-user/point/app-user-point-history.entity';
-import { AppUserPointStorage } from '@domain/app-user/point/app-user-point-storage.entity';
-import { AppUserPoint } from '@domain/app-user/point/app-user-point.entity';
-import { AppUserPointConsumption } from '@domain/app-user/point/app-user-point-consumption.entity';
-import { UserAddress } from '../domain/user/address/user-address.entity';
+import { join } from 'path';
 
 @Module({
   imports: [
@@ -27,12 +22,7 @@ import { UserAddress } from '../domain/user/address/user-address.entity';
       password: process.env.DB_PASSWORD,
       database: process.env.DB_DATABASE,
       entities: [
-        AppUser,
-        AppUserPoint,
-        AppUserPointHistory,
-        AppUserPointStorage,
-        AppUserPointConsumption,
-        UserAddress,
+        join(__dirname, '../../../../libs/domain/src/**/*.entity.js'), // domain
       ],
       synchronize: true,
     }),
