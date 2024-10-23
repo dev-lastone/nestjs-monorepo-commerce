80f7234a28b4cb88526d58d4bce1cc40b87b916f
jjong <akrTmwk@gmail.com>
2024-10-23 23:38:19 +0900

    refactor: AppUser entity column 정의

--- libs/domain/src/app-user/app-user.entity.ts
+++ libs/domain/src/app-user/app-user.entity.ts
@@ -1,6 +1,6 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
-import { Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity('user', { schema: 'app' })
 export class AppUser {
@@ -9,12 +9,15 @@ export class AppUser {
   @ApiProperty({ default: '홍길동' })
   @IsNotEmpty()
   @IsString()
+  @Column({ name: 'name', type: 'varchar', length: 30 })
   name: string;
   @ApiProperty({ default: 'test@test.com' })
   @IsEmail()
   @IsNotEmpty()
+  @Column({ name: 'email', type: 'varchar', length: 100 })
   email: string;
   @ApiProperty({ default: '1234' })
   @IsNotEmpty()
+  @Column({ name: 'password', type: 'varchar', length: 50 })
   password: string;
 }
