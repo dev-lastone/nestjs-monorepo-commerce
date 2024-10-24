e4d2ad4828318bf15a03d08272f7dcd58fdbe4ac
jongwon-choi <90lastone@gmail.com>
2024-10-24 14:35:33 +0900

    refactor: ProductLike entity 및 repo 적용

---
+++ apps/app/src/domain/product/like/product-like.repo.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { ProductLike } from './product-like.entity';
+
+@Injectable()
+export class ProductLikeRepo {
+  constructor(
+    @InjectRepository(ProductLike)
+    private readonly productLikeRepo: Repository<ProductLike>,
+  ) {}
+
+  async save(productLike: ProductLike) {
+    return await this.productLikeRepo.save(productLike);
+  }
+
+  async delete(id: number) {
+    return await this.productLikeRepo.delete(id);
+  }
+
+  async findOne(partial: Partial<ProductLike>) {
+    return await this.productLikeRepo.findOne({
+      where: {
+        ...partial,
+      },
+    });
+  }
+}
