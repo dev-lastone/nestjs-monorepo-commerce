20b1808db67c1465760398a23a6c39c0f8af4f4c
jjong <akrTmwk@gmail.com>
2024-10-24 22:47:12 +0900

    refactor: User UserAddress entity 관계 정의

--- apps/app/src/domain/user/address/__stub/user-address.stub.ts
+++ apps/app/src/domain/user/address/__stub/user-address.stub.ts
@@ -1,9 +1,12 @@
 import { appUserStub } from '@domain/app-user/__stub/app-user.stub';
+import { UserAddress } from '../user-address.entity';
 
-export const userAddressStub = {
-  id: 1,
+const userAddress = UserAddress.create({
   userId: appUserStub.id,
   zipcode: '01234',
   address: '서울시 강남구 역삼동 *********',
   isDefault: true,
-};
+});
+userAddress.id = 1;
+
+export const userAddressStub = userAddress;
