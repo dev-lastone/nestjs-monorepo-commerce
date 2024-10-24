108499a13ffa7fd791ceccdfcbb6bc77bf51beb4
jongwon-choi <90lastone@gmail.com>
2024-10-24 16:08:29 +0900

    refactor: package jest 설정 파일 분리. orm db 연결로직 추가로, 단위테스트시 jest ignore e2e, module 추가

---
+++ jest.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  moduleFileExtensions: ['js', 'json', 'ts'],
+  rootDir: '.',
+  testMatch: ['**/*.spec.ts'],
+  transform: {
+    '^.+\\.(t|j)s$': 'ts-jest',
+  },
+  // TODO unit, integration, e2e 테스트 환경별로 설정을 나눠야함
+  coveragePathIgnorePatterns: [
+    'main.ts',
+    'common/src/setting/*',
+    '.repo.ts$',
+    '/node_modules/',
+    '.e2e-spec.ts$',
+    '.module.ts$',
+  ],
+  collectCoverageFrom: ['**/*.(t|j)s'],
+  coverageDirectory: './coverage',
+  testEnvironment: 'node',
+  roots: ['<rootDir>/apps/', '<rootDir>/libs/'],
+  moduleNameMapper: {
+    '^@domain(|/.*)$': '<rootDir>/libs/domain/src/$1',
+    '^@common(|/.*)$': '<rootDir>/libs/common/src/$1',
+    '^@application(|/.*)$': '<rootDir>/libs/application/src/$1',
+  },
+};
