89a1a0628e96ca7f126a95f8a6da8b2733b259a5
jjong <akrTmwk@gmail.com>
2024-10-24 22:37:42 +0900

    refactor: productStub Product.create 적용

--- libs/domain/src/product/__stub/product.stub.ts
+++ libs/domain/src/product/__stub/product.stub.ts
@@ -1,14 +1,19 @@
-export const productStub1 = {
-  id: 1,
+import { Product } from '@domain/product/product.entity';
+
+export const product1 = Product.create({
   name: '상품명1',
   price: 10000,
   stock: 10,
-};
-export const productStub2 = {
-  id: 2,
+});
+product1.id = 1;
+export const productStub1 = product1;
+
+export const product2 = Product.create({
   name: '상품명2',
   price: 20000,
-  stock: 1,
-};
+  stock: 20,
+});
+product2.id = 2;
+export const productStub2 = product2;
 
 export const productsStub = [productStub1, productStub2];
