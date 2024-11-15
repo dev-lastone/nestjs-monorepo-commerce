2afd24b6d2623f1450ccfeae9ec4bda81553ad50
jongwon-choi <90lastone@gmail.com>
2024-11-15 14:35:35 +0900

    refactor: test vo user-name App, AdminUser 적용

--- libs/domain/src/admin-user/__stub/admin-user.stub.ts
+++ libs/domain/src/admin-user/__stub/admin-user.stub.ts
@@ -1,8 +1,13 @@
+import { UserName } from '@domain/_vo/user-name';
+
 export const adminUserStub = {
   id: 1,
-  name: '홍길동',
+  name: UserName.create('홍길동'),
   email: 'test@test.com',
   password: '1234',
+  createdAt: new Date(),
+  updatedAt: new Date(),
+  deletedAt: null,
 };
 
 export const invalidAdminUserStub = {
