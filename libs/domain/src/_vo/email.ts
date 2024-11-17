c355b9cd772a92fe0ae788163c5ed013faf05c25
jjong <akrTmwk@gmail.com>
2024-11-17 15:28:32 +0900

    refactor: vo Email. requestDto 적용

--- libs/domain/src/_vo/email.ts
+++ libs/domain/src/_vo/email.ts
@@ -1,27 +1,36 @@
 import { Column } from 'typeorm';
-import { IsEmail, IsNotEmpty, validateOrReject } from 'class-validator';
-import { ApiProperty } from '@nestjs/swagger';
+import { IsEmail, IsNotEmpty, validateSync } from 'class-validator';
+import { BadRequestException, HttpStatus } from '@nestjs/common';
 
 export class Email {
-  @ApiProperty({ example: 'test@test.com' })
   @IsEmail()
   @IsNotEmpty()
   @Column({ name: 'email', type: 'varchar', length: 100, unique: true })
   private readonly value: string;
 
-  constructor(email: string) {
-    this.value = email;
+  constructor(
+    value: string,
+    options?: {
+      httpStatus: HttpStatus;
+    },
+  ) {
+    this.value = value;
+
+    const errors = validateSync(this);
+    if (errors.length > 0) {
+      const errorConstraints = errors.map((error) => {
+        return error.constraints;
+      });
+
+      if (options.httpStatus === HttpStatus.BAD_REQUEST) {
+        throw new BadRequestException(errorConstraints);
+      } else {
+        throw new Error(JSON.stringify(errorConstraints));
+      }
+    }
   }
 
-  static create(email: string) {
-    const _email = new Email(email);
-
-    this.validate();
-
-    return _email;
-  }
-
-  static async validate() {
-    await validateOrReject(this);
+  static create(value: string) {
+    return new Email(value);
   }
 }
