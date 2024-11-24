740a57875f608defb2ac8e52c943120a876d5907
jjong <akrTmwk@gmail.com>
2024-11-24 21:04:10 +0900

    refactor: UserPassword 값 검증 데코레이터. 커스텀 정의 및 적용

--- apps/admin/src/api/auth/auth.admin.dto.ts
+++ apps/admin/src/api/auth/auth.admin.dto.ts
@@ -1,30 +1,16 @@
-import { ApiProperty, PickType } from '@nestjs/swagger';
+import { PickType } from '@nestjs/swagger';
 import { AdminUser } from '@domain/admin-user/admin-user.entity';
-import { IsNotEmpty, Length } from 'class-validator';
+import { UserPasswordValidation } from '@common/decorator/user-password-validation.decorator';
 
 export class PostAuthAdminSignUpReqDto extends PickType(AdminUser, [
   'name',
   'email',
 ]) {
-  @ApiProperty({
-    example: 'string1234',
-    description: '비밀번호',
-    minLength: 8,
-    maxLength: 20,
-  })
-  @IsNotEmpty()
-  @Length(8, 20)
+  @UserPasswordValidation()
   password: string;
 }
 
 export class PostAuthAdminRequestDto extends PickType(AdminUser, ['email']) {
-  @ApiProperty({
-    example: 'string1234',
-    description: '비밀번호',
-    minLength: 8,
-    maxLength: 20,
-  })
-  @IsNotEmpty()
-  @Length(8, 20)
+  @UserPasswordValidation()
   password: string;
 }
