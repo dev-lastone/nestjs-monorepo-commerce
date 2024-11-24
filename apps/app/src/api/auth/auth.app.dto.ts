05ba2342ff5128c4b2c92f2d22482e80d29897a0
jjong <akrTmwk@gmail.com>
2024-11-24 21:49:09 +0900

    refactor: AuthAppDto UserPasswordValidation 데코레이터 적용

--- apps/app/src/api/auth/auth.app.dto.ts
+++ apps/app/src/api/auth/auth.app.dto.ts
@@ -1,32 +1,13 @@
-import { ApiProperty, PickType } from '@nestjs/swagger';
-import { AppUser } from '@domain/app-user/app-user.entity';
-import { IsNotEmpty, Length } from 'class-validator';
+import { PickType } from '@nestjs/swagger';
+import { User } from '@domain/_vo/user';
+import { UserPasswordValidation } from '@common/decorator/user-password-validation.decorator';
 
-export class PostAuthSignUpAppReqDto extends PickType(AppUser, [
-  'name',
-  'email',
-]) {
-  // TODO custom decorator 만들기
-  // TODO 특문 조합 룰 추가
-  @ApiProperty({
-    example: 'string1234',
-    description: '비밀번호',
-    minLength: 8,
-    maxLength: 20,
-  })
-  @IsNotEmpty()
-  @Length(8, 20)
+export class PostAuthSignUpAppReqDto extends PickType(User, ['name', 'email']) {
+  @UserPasswordValidation()
   password: string;
 }
 
-export class PostAuthAppRequestDto extends PickType(AppUser, ['email']) {
-  @ApiProperty({
-    example: 'string1234',
-    description: '비밀번호',
-    minLength: 8,
-    maxLength: 20,
-  })
-  @IsNotEmpty()
-  @Length(8, 20)
+export class PostAuthAppRequestDto extends PickType(User, ['email']) {
+  @UserPasswordValidation()
   password: string;
 }
