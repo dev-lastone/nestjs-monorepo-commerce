ce84e236c4b248b3cd744574972ac269e3657755
jongwon-choi <90lastone@gmail.com>
2024-11-26 15:47:28 +0900

    refactor: app test order -> test unit 으로 이동

--- apps/app/test/unit/order/order-products/order-products.app.controller.spec.ts
+++ apps/app/test/unit/order/order-products/order-products.app.controller.spec.ts
@@ -1,7 +1,7 @@
 import { Test } from '@nestjs/testing';
-import { OrderProductsAppController } from '../../order-products/order-products.app.controller';
 import { appUserStub } from '@domain/app-user/__stub/app-user.stub';
 import { OrderApplicationService } from '@application/order/order.application.service';
+import { OrderProductsAppController } from '../../../../src/api/order/order-products/order-products.app.controller';
 
 describe('OrderProductsAppController', () => {
   let orderProductsAppController: OrderProductsAppController;
