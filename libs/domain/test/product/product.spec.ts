2fb838213c632a7d593bf8416e76535b424cc9ae
jongwon-choi <90lastone@gmail.com>
2024-11-29 13:30:48 +0900

    refactor: test Product async 미사용 제거

--- libs/domain/test/product/product.spec.ts
+++ libs/domain/test/product/product.spec.ts
@@ -2,14 +2,14 @@ import { CreateProductDto } from '@domain/product/dto/product.dto';
 import { Product } from '@domain/product/product.entity';
 
 describe('Product', () => {
-  it('create', async () => {
+  it('create', () => {
     const dto: CreateProductDto = {
       name: '홍길동',
       price: 12000,
       stock: 10,
     };
 
-    const product = await Product.create(dto);
+    const product = Product.create(dto);
 
     expect(product).toBeInstanceOf(Product);
     expect(product).toEqual(dto);
