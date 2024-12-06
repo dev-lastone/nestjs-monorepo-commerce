0571b49f71e1c8e192665b89f93926acc7fe8647
jongwon-choi <90lastone@gmail.com>
2024-12-06 17:06:52 +0900

    test: UserPassword create fail

--- libs/domain/test/_vo/user-password.spec.ts
+++ libs/domain/test/_vo/user-password.spec.ts
@@ -1,19 +1,31 @@
 import { UserPassword } from '@domain/_vo/user-password';
 
 describe('UserPassword', () => {
-  const value = 'string1234';
+  const validValue = 'string1234';
 
-  it('create', async () => {
-    const userPassword = await UserPassword.create(value);
-    expect(userPassword).toBeInstanceOf(UserPassword);
-    expect(userPassword.getValue()).not.toBe(value);
-    expect(userPassword.getValue()).toHaveLength(60);
+  describe('create', () => {
+    const shortValue = '1234567';
+    it('should fail validation when password is short', () => {
+      expect(() => UserPassword.create(shortValue)).rejects.toHaveLength(1);
+    });
+
+    it('should fail validation when password is long', () => {
+      const longValue = '123456789012345678901';
+      expect(() => UserPassword.create(longValue)).rejects.toHaveLength(1);
+    });
+
+    it('should create a UserPassword when the input is valid', async () => {
+      const userPassword = await UserPassword.create(validValue);
+      expect(userPassword).toBeInstanceOf(UserPassword);
+      expect(userPassword.getValue()).not.toBe(validValue);
+      expect(userPassword.getValue()).toHaveLength(60);
+    });
   });
 
   it('compare', async () => {
-    const userPassword = await UserPassword.create(value);
+    const userPassword = await UserPassword.create(validValue);
 
-    await expect(userPassword.compare(value)).resolves.not.toThrow();
+    await expect(userPassword.compare(validValue)).resolves.not.toThrow();
     await expect(userPassword.compare('invalid')).rejects.toThrow();
   });
 });
