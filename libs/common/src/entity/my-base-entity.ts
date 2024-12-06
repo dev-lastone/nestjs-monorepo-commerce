49d1a5de2aaafc54c9a95dd49e62df993e5d6ae3
jongwon-choi <90lastone@gmail.com>
2024-12-06 17:32:12 +0900

    refactor: MyBaseEntity 의미없는 default 옵션 제거

--- libs/common/src/entity/my-base-entity.ts
+++ libs/common/src/entity/my-base-entity.ts
@@ -13,14 +13,11 @@ export class MyBaseEntity {
 
   @CreateDateColumn({
     type: 'timestamp',
-    default: () => 'CURRENT_TIMESTAMP',
   })
   createdAt: Date;
 
   @UpdateDateColumn({
     type: 'timestamp',
-    default: () => 'CURRENT_TIMESTAMP',
-    onUpdate: 'CURRENT_TIMESTAMP',
   })
   updatedAt: Date;
 
