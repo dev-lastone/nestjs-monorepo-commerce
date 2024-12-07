cfcc9c33b5f59e44d6cf351bfab3cd767f0f5062
jjong <akrTmwk@gmail.com>
2024-12-07 16:27:21 +0900

    refactor: AuthAppService AppService 분리

--- libs/application/src/app-user/app-user.module.ts
+++ libs/application/src/app-user/app-user.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppUserRepo } from './app-user.repo';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppUser } from '@domain/app-user/app-user.entity';
+import { AppUserService } from '@application/app-user/app-user.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([AppUser])],
-  providers: [AppUserRepo],
-  exports: [AppUserRepo],
+  providers: [AppUserService, AppUserRepo],
+  exports: [AppUserService, AppUserRepo],
 })
 export class AppUserModule {}
