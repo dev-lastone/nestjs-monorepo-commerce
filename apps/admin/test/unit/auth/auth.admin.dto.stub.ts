58f6460a0a15cfe63eeeb4c6c13e6c51d252e54b
jjong <akrTmwk@gmail.com>
2024-12-07 16:32:39 +0900

    refactor: AuthAdminDtoStub invalidUserSignInDto 미사용 제거

--- apps/admin/test/unit/auth/auth.admin.dto.stub.ts
+++ apps/admin/test/unit/auth/auth.admin.dto.stub.ts
@@ -2,8 +2,3 @@ export const postAuthAppRequestDtoStub = {
   email: 'test@test.com',
   password: 'string1234',
 };
-
-export const invalidUserSignInDto = {
-  email: 'invalid@test.come',
-  password: '1234',
-};
