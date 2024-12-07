6e7aac4a64252b89e4f9176a0dbc91a318a86cfe
jjong <akrTmwk@gmail.com>
2024-12-07 15:47:12 +0900

    refactor: admin user signIn. AdminUserService 로 로직 추출

--- apps/admin/src/api/auth/auth.admin.service.ts
+++ apps/admin/src/api/auth/auth.admin.service.ts
@@ -1,8 +1,6 @@
-import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { PostAuthAdminRequestDto } from './auth.admin.dto';
-import { AdminUserRepo } from '@application/admin-user/admin-user.repo';
 import { AuthService } from '@application/auth/auth.service';
-import { ERROR_MESSAGES } from '@common/constant/error-messages';
 import { CreateUserDto } from '@domain/_vo/dto/create-user.dto';
 import { AdminUserService } from '@application/admin-user/admin-user.service';
 
@@ -11,7 +9,6 @@ export class AuthAdminService {
   constructor(
     private readonly authService: AuthService,
     private readonly adminUserService: AdminUserService,
-    private readonly adminUserRepo: AdminUserRepo,
   ) {}
 
   async signUp(dto: CreateUserDto) {
@@ -20,14 +17,7 @@ export class AuthAdminService {
   }
 
   async signIn(dto: PostAuthAdminRequestDto) {
-    const adminUser = await this.adminUserRepo.findOneByEmail(dto.email);
-
-    if (!adminUser) {
-      throw new UnauthorizedException(ERROR_MESSAGES.InvalidSignIn);
-    }
-
-    await adminUser.user.password.compare(dto.password);
-
+    const adminUser = await this.adminUserService.signIn(dto);
     return this.authService.createToken(adminUser);
   }
 }
--- libs/application/src/admin-user/admin-user.service.ts
+++ libs/application/src/admin-user/admin-user.service.ts
@@ -1,8 +1,13 @@
-import { BadRequestException, Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { AdminUserRepo } from '@application/admin-user/admin-user.repo';
 import { ERROR_MESSAGES } from '@common/constant/error-messages';
 import { AdminUser } from '@domain/admin-user/admin-user.entity';
 import { CreateUserDto } from '@domain/_vo/dto/create-user.dto';
+import { PostAuthAdminRequestDto } from '../../../../apps/admin/src/api/auth/auth.admin.dto';
 
 @Injectable()
 export class AdminUserService {
@@ -17,5 +22,17 @@ export class AdminUserService {
     const user = await AdminUser.create(dto);
 
     return await this.adminUserRepo.save(user);
+  }
+
+  async signIn(dto: PostAuthAdminRequestDto) {
+    const adminUser = await this.adminUserRepo.findOneByEmail(dto.email);
+
+    if (!adminUser) {
+      throw new UnauthorizedException(ERROR_MESSAGES.InvalidSignIn);
+    }
+
+    await adminUser.user.password.compare(dto.password);
+
+    return adminUser;
   }
 }
