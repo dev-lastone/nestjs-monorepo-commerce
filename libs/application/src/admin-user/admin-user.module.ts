4c9672b356548534489b2810a400335407a402c2
jjong <akrTmwk@gmail.com>
2024-12-07 15:07:42 +0900

    refactor: admin user signUp. AdminUserService 로 로직 추출

--- libs/application/src/admin-user/admin-user.module.ts
+++ libs/application/src/admin-user/admin-user.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AdminUserRepo } from '@application/admin-user/admin-user.repo';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AdminUser } from '@domain/admin-user/admin-user.entity';
+import { AdminUserService } from '@application/admin-user/admin-user.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([AdminUser])],
-  providers: [AdminUserRepo],
-  exports: [AdminUserRepo],
+  providers: [AdminUserService, AdminUserRepo],
+  exports: [AdminUserService, AdminUserRepo],
 })
 export class AdminUserModule {}
