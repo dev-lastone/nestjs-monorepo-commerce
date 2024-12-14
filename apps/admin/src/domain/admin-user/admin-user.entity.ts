7373975bb386019bd0c29446e623345d5bb7efa6
jjong <akrTmwk@gmail.com>
2024-12-14 21:00:08 +0900

    refactor: AdminUser 주석 제거

--- apps/admin/src/domain/admin-user/admin-user.entity.ts
+++ apps/admin/src/domain/admin-user/admin-user.entity.ts
@@ -2,7 +2,6 @@ import { Entity } from 'typeorm';
 import { User } from '@domain/user/user';
 import { CreateUserDto } from '@domain/user/dto/create-user.dto';
 
-// TODO admin jwt 별도 발급시 admin domain 으로 이동 예정
 @Entity('user', { schema: 'admin' })
 export class AdminUser extends User {
   static async create(dto: CreateUserDto) {
