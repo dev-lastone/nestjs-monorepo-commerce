10c2db284452d9a3f8d609096121a84f08ea8c13
jjong <akrTmwk@gmail.com>
2024-12-16 15:06:54 +0900

    feat: AppUserPoint. history, storage, consumption repo 주입

--- libs/application/src/app-user-point/app-user-point.repo.ts
+++ libs/application/src/app-user-point/app-user-point.repo.ts
@@ -2,12 +2,21 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { AppUserPoint } from '@domain/app-user/point/app-user-point.entity';
+import { AppUserPointHistory } from '@domain/app-user/point/app-user-point-history.entity';
+import { AppUserPointStorage } from '@domain/app-user/point/app-user-point-storage.entity';
+import { AppUserPointConsumption } from '@domain/app-user/point/app-user-point-consumption.entity';
 
 @Injectable()
 export class AppUserPointRepo {
   constructor(
     @InjectRepository(AppUserPoint)
     private readonly appUserPointRepo: Repository<AppUserPoint>,
+    @InjectRepository(AppUserPointHistory)
+    private readonly appUserPointHistoryRepo: Repository<AppUserPointHistory>,
+    @InjectRepository(AppUserPointStorage)
+    private readonly appUserPointStorageRepo: Repository<AppUserPointStorage>,
+    @InjectRepository(AppUserPointConsumption)
+    private readonly appUserPointConsumptionRepo: Repository<AppUserPointConsumption>,
   ) {}
 
   async save(appUserPoint: AppUserPoint) {
