33fb054d3fc0af661ebac34840b414bf30526997
jjong <akrTmwk@gmail.com>
2024-12-16 16:46:42 +0900

    refactor: MyBaseEntity db column name 명시

--- libs/common/src/entity/my-base-entity.ts
+++ libs/common/src/entity/my-base-entity.ts
@@ -12,16 +12,19 @@ export class MyBaseEntity {
   id: number;
 
   @CreateDateColumn({
+    name: 'created_at',
     type: 'timestamp',
   })
   createdAt: Date;
 
   @UpdateDateColumn({
+    name: 'updated_at',
     type: 'timestamp',
   })
   updatedAt: Date;
 
   @DeleteDateColumn({
+    name: 'deleted_at',
     type: 'timestamp',
     nullable: true,
   })
