be19c0ec1bbefdde7f29e66b6073c10574557602
jjong <akrTmwk@gmail.com>
2024-12-21 11:52:45 +0900

    refactor: userPasswordStub userStub 전용사용으로 이동

--- libs/domain/test/_vo/_stub/user-password.stub.ts
+++ libs/domain/test/_vo/_stub/user-password.stub.ts
@@ -1,7 +1,1 @@
-import { UserPassword } from '@domain/_vo/user-password';
-
-export const userPasswordStub = {
-  async compare(password: string) {},
-} as UserPassword;
-
 export const invalidPasswordStub = 'invalidPassword';
