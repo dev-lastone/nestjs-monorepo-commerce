aaa6ee231c20ea87cd28334077d7d41ebb149d11
jjong <akrTmwk@gmail.com>
2025-02-07 21:16:59 +0900

    feat: OrderProductHistory entity 구성

---
+++ libs/domain/src/order/order-product-history.entity.ts
@@ -0,0 +1,37 @@
+import { Column, Entity, JoinColumn, ManyToOne } from 'typeorm';
+import { dtoToInstance } from '@common/util/dto-to-instance';
+import { BigIntToNumberTransformer } from '@common/entity/transformer';
+import {
+  OrderProduct,
+  OrderProductStatus,
+} from '@domain/order/order-product.entity';
+import { IsNotEmpty, MaxLength } from 'class-validator';
+import { MyBaseEntity } from '@common/entity/my-base-entity';
+
+@Entity('order_product_history', { schema: 'app' })
+export class OrderProductHistory extends MyBaseEntity {
+  @Column({
+    name: 'order_product_id',
+    type: 'bigint',
+    transformer: BigIntToNumberTransformer,
+  })
+  orderProductId: number;
+
+  @IsNotEmpty()
+  @MaxLength(20)
+  @Column({ name: 'status', type: 'varchar', length: 20 })
+  status: OrderProductStatus;
+
+  @ManyToOne(() => OrderProduct, (orderProduct) => orderProduct.histories)
+  @JoinColumn({ name: 'order_product_id', referencedColumnName: 'id' })
+  orderProduct: OrderProduct;
+
+  static create(orderProduct: OrderProduct) {
+    return dtoToInstance({
+      class: OrderProductHistory,
+      dto: {
+        status: orderProduct.status,
+      },
+    });
+  }
+}
