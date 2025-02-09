251c5829db3437891a0ff3886a5061ab05e7ad3b
jjong <akrTmwk@gmail.com>
2025-02-09 21:36:52 +0900

    feat: 배치 주문 자동 확정 조회

--- apps/batch/src/order/order.batch.module.ts
+++ apps/batch/src/order/order.batch.module.ts
@@ -1,7 +1,11 @@
 import { Module } from '@nestjs/common';
 import { OrderBatchService } from './order.batch.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { OrderProduct } from '@domain/order/order-product.entity';
+import { OrderBatchRepo } from './order.batch.repo';
 
 @Module({
-  providers: [OrderBatchService],
+  imports: [TypeOrmModule.forFeature([OrderProduct])],
+  providers: [OrderBatchService, OrderBatchRepo],
 })
 export class OrderBatchModule {}
--- apps/batch/src/order/order.batch.service.ts
+++ apps/batch/src/order/order.batch.service.ts
@@ -1,11 +1,18 @@
 import { Injectable } from '@nestjs/common';
 import { Cron } from '@nestjs/schedule';
+import { OrderBatchRepo } from './order.batch.repo';
 
 @Injectable()
 export class OrderBatchService {
+  constructor(private readonly orderBatchRepo: OrderBatchRepo) {}
+
   @Cron('0 0 0 * * *')
-  confirmOrders() {
-    // 배송일 부터 7일 지난 주문상품들 조회
+  async confirmOrdersAutomatically() {
+    const orderProducts =
+      await this.orderBatchRepo.findOrderProductsToBeConfirmed();
     // 구매확정 로직(상태변경, 포인트 적립)
+    orderProducts.map((orderProduct) => {
+      orderProduct.confirm();
+    });
   }
 }
