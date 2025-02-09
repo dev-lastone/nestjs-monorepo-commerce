be1b7c83fceaa03492ffc6ddb74174b3db390fd8
jjong <akrTmwk@gmail.com>
2025-02-09 21:28:29 +0900

    refactor: OrderProductHistory entity updatedAt, deletedAt 미사용 제거

--- libs/domain/src/order/order-product-history.entity.ts
+++ libs/domain/src/order/order-product-history.entity.ts
@@ -1,4 +1,10 @@
-import { Column, Entity, JoinColumn, ManyToOne } from 'typeorm';
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+} from 'typeorm';
 import { dtoToInstance } from '@common/util/dto-to-instance';
 import { BigIntToNumberTransformer } from '@common/entity/transformer';
 import {
@@ -6,10 +12,13 @@ import {
   OrderProductStatus,
 } from '@domain/order/order-product.entity';
 import { IsNotEmpty, MaxLength } from 'class-validator';
-import { MyBaseEntity } from '@common/entity/my-base-entity';
+import { PrimaryGeneratedBigintColumn } from '@common/decorator/primary-generated-bigint-column.decorator';
 
 @Entity('order_product_history', { schema: 'app' })
-export class OrderProductHistory extends MyBaseEntity {
+export class OrderProductHistory {
+  @PrimaryGeneratedBigintColumn()
+  id: number;
+
   @Column({
     name: 'order_product_id',
     type: 'bigint',
@@ -25,6 +34,12 @@ export class OrderProductHistory extends MyBaseEntity {
   @ManyToOne(() => OrderProduct, (orderProduct) => orderProduct.histories)
   @JoinColumn({ name: 'order_product_id', referencedColumnName: 'id' })
   orderProduct: OrderProduct;
+
+  @CreateDateColumn({
+    name: 'created_at',
+    type: 'timestamp',
+  })
+  createdAt: Date;
 
   static create(orderProduct: OrderProduct) {
     return dtoToInstance({
