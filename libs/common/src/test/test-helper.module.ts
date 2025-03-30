51624ac776034506325811f9dbae7557a59675af
jjong <akrTmwk@gmail.com>
2025-03-30 10:35:48 +0900

    refactor: 테스트 헬퍼 모듈 typeorm setting admin 분리

--- libs/common/src/test/test-helper.module.ts
+++ libs/common/src/test/test-helper.module.ts
@@ -1,11 +1,15 @@
-import { Module } from '@nestjs/common';
+import { DynamicModule, Module } from '@nestjs/common';
 import { TestHelperService } from './test-helper.service';
 import { configModule } from '@common/setting/config';
-import { typeOrmAdminTestSetting } from '@common/setting/type-orm.setting';
 
-@Module({
-  imports: [configModule(), typeOrmAdminTestSetting()],
-  providers: [TestHelperService],
-  exports: [TestHelperService],
-})
-export class TestHelperModule {}
+@Module({})
+export class TestHelperModule {
+  static forRoot(typeOrmSetting: () => DynamicModule): DynamicModule {
+    return {
+      module: TestHelperModule,
+      imports: [configModule(), typeOrmSetting()],
+      providers: [TestHelperService],
+      exports: [TestHelperService],
+    };
+  }
+}
